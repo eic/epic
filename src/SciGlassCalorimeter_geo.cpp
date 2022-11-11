d06d3d6c149e8419a3d7829a6a7c9b45a40637a3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-11-10 21:58:11 -0500

    SciGlassCalorimeter_geo: remove a redundant license header

--- src/SciGlassCalorimeter_geo.cpp
+++ src/SciGlassCalorimeter_geo.cpp
@@ -1,26 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2022 Dmitry Kalinkin, Wouter Deconinck
 
-/*
- * This file is part of EPIC Sci-Glass BECal Detector Description.
- *
- * EPIC Sci-Glass BECal Detector Description is free software: you can
- * redistribute it and/or modify it under the terms of the GNU Lesser
- * General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * EPIC Sci-Glass BECal Detector Description is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file. If not, see
- * <https://www.gnu.org/licenses/>.
- */
-
 #include <array>
 #include <limits>
 #include <string>
