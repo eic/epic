43c29daf70da39f7a60582886dd555a6362963f2
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-11-30 18:57:14 -0500

    update copyright notice

--- src/DRICH_geo.cpp
+++ src/DRICH_geo.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2022 Christopher Dilks, Junhuai Xu
+
 //==========================================================================
 //  dRICH: Dual Ring Imaging Cherenkov Detector
 //--------------------------------------------------------------------------
