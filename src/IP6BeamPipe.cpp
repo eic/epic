a61eb3fa74d33a0edb2142c322bdf68f06907c7b
Sakib Rahman <rahmans@myumanitoba.ca>
2022-12-02 13:28:48 -0600

    Fix typo

--- src/IP6BeamPipe.cpp
+++ src/IP6BeamPipe.cpp
@@ -73,7 +73,7 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
   DetElement   central_det(sdet, "acts_beampipe_central", 1);
 
   dd4hep::xml::setDetectorTypeFlag(xml, central_det);
-  auto &params = ODDHelper::ensureExtension<dd4hep::rec::VariantParameters>(
+  auto &params = DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(
       central_det);
 
   for (xml_coll_t lmat(x_det_tubs, _Unicode(layer_material)); lmat; ++lmat) {
