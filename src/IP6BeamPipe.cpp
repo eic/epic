352891b8de25377cfb90fc9e70d5a12db1b9063e
Sakib Rahman <rahmans@myumanitoba.ca>
2022-12-03 11:41:48 -0600

    Set the type flag to the central tube detector element because only that will be passed to ACTS
    The central tube is the only portion of beam pipe that has tracking detectors around it. So, only that is passed to ACTS.

--- src/IP6BeamPipe.cpp
+++ src/IP6BeamPipe.cpp
@@ -72,6 +72,8 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
   const double central_offset = -.5 * (upstream_straight_length - downstream_straight_length);
   DetElement   central_det(sdet, "acts_beampipe_central", 1);
 
+  // Set dd4hep variant parameters for conversion to ACTS tracking geometry
+  dd4hep::xml::setDetectorTypeFlag(e, central_det);
   auto &params = DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(central_det);
   int nBinPhi = 144; // fix later. Should take this from a xml tag
   int nBinZ = 10;  // fix later. Should take this from a xml tag
