4630b5406410641652e54a0ea8b683d8dcc2bdb3
Sakib Rahman <rahmans@myumanitoba.ca>
2022-12-04 17:54:01 -0600

    Adding axis definitions variant parameter for elements in trap end cap tracker
    Previously this was set by ACTS extension. Now using variant parameters.

--- src/TrapEndcapTracker_geo.cpp
+++ src/TrapEndcapTracker_geo.cpp
@@ -276,6 +276,8 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
           for (size_t ic = 0; ic < sensVols.size(); ++ic) {
             PlacedVolume sens_pv = sensVols[ic];
             DetElement   comp_elt(module, sens_pv.volume().name(), mod_num);
+            auto &params = DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(comp_elt);
+            params.set("axis_definitions", "XZY");
             comp_elt.setPlacement(sens_pv);
             volSurfaceList(comp_elt)->push_back(volplane_surfaces[m_nam][ic]);
           }
@@ -288,6 +290,8 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
           for (size_t ic = 0; ic < sensVols.size(); ++ic) {
             PlacedVolume sens_pv = sensVols[ic];
             DetElement   comp_elt(r_module, sens_pv.volume().name(), mod_num);
+            auto &params = DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(comp_elt);
+            params.set("axis_definitions", "XZY");
             comp_elt.setPlacement(sens_pv);
             volSurfaceList(comp_elt)->push_back(volplane_surfaces[m_nam][ic]);
           }
