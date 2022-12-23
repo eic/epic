8693f3abc5d5210dc9e4de833452b5ead5b1b934
Sakib Rahman <rahmans@myumanitoba.ca>
2022-12-22 23:19:05 -0600

    Fix typo

--- src/B0Tracker_geo.cpp
+++ src/B0Tracker_geo.cpp
@@ -170,8 +170,8 @@ static Ref_t create_B0Tracker(Detector& description, xml_h e, SensitiveDetector 
     std::string layer_vis = l_env.attr<std::string>(_Unicode(vis));
     double      layer_rmin_tolerance   = l_env.attr<double>(_Unicode(rmin_tolerance));
     double      layer_rmax_tolerance  = l_env.attr<double>(_Unicode(rmax_tolerance));
-    double      layer_rmin_tolerance   = l_env.attr<double>(_Unicode(zmin_tolerance));
-    double      layer_rmax_tolerance  = l_env.attr<double>(_Unicode(zmax_tolerance));
+    double      layer_zmin_tolerance   = l_env.attr<double>(_Unicode(zmin_tolerance));
+    double      layer_zmax_tolerance  = l_env.attr<double>(_Unicode(zmax_tolerance));
     double layer_length   = l_env.attr<double>(_Unicode(length));
     double layer_zstart   = l_env.attr<double>(_Unicode(zstart));
     double layer_center_z = layer_zstart + layer_length / 2.0;
