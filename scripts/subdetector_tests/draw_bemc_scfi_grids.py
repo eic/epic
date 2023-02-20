67ac6d8758cfb012f2230102aa13a58a555e5cf5
Chao Peng <cpeng@anl.gov>
2023-02-20 17:55:28 -0600

    follow up: finish revamping the visualization script

--- scripts/subdetector_tests/draw_bemc_scfi_grids.py
+++ scripts/subdetector_tests/draw_bemc_scfi_grids.py
@@ -9,6 +9,7 @@ import dd4hep
 import DDRec
 import argparse
 import numpy as np
+from pydoc import locate
 from matplotlib import pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
@@ -20,30 +21,41 @@ from collections import OrderedDict
 def dict_to_cpp_vec(my_dict, dtype='int'):
     # use ROOT to make sure the type is correct
     vol_ids = ROOT.std.vector('pair<string, {}>'.format(dtype))()
+    dcast = locate(dtype)
     for field, fid in my_dict.items():
-        vol_ids.push_back((field, fid))
+        vol_ids.push_back((field, dcast(fid)))
     return vol_ids
 
 
 # helper function to collect fibers under a grid
-def get_fibers(detelem, id_desc, id_conv, id_dict, grid=1):
-    gnode = detelem.volume().GetNode(grid-1)
-    global_trans = detelem.nominal().worldTransformation()
-    # print(gnode.GetName())
-    # get grid
-    id_dict.update({'grid': grid, 'fiber': 0})
+def get_grid_fibers(det_elem, vol_man, id_conv, id_dict):
+    # locate the nearest DetElement
+    id_desc = vol_man.idSpec()
+    # get fiber radius
+    id_dict.update({'fiber': 1})
+    fid = id_desc.encode(dict_to_cpp_vec(id_dict))
+    # NOTE: for tube geometry, and it needs a cm to mm conversion
+    fr = id_conv.cellDimensions(fid)[0]/2./10.
+
+    # get the lowest level DetElement
+    sdet = id_conv.findDetElement(id_conv.position(fid))
+    gtrans = sdet.nominal().worldTransformation()
+
+    # get grid node (it's not a DetElement)
+    id_dict.update({'fiber': 0})
     gid = id_desc.encode(dict_to_cpp_vec(id_dict))
-    # print(id_desc.decoder().valueString(vid))
-    vol = id_conv.findContext(gid).volumePlacement().volume()
+    gnode = id_conv.findContext(gid).volumePlacement()
+    # print(id_desc.decoder().valueString(gid))
     grpos = id_conv.position(gid)
-    # print(vol.boundingBox().dimensions())
+    grpos = np.array([grpos.X(), grpos.Y(), grpos.Z()])
 
     # use TGeoNode to get center positions
-    # here it can also use converter to get position but it's much slower (adds an additional lookup process)
+    # here it can also use id_conv to do the same thing with cellIDs,
+    # but it's much slower (adds an additional lookup process)
     fibers = []
-    for i in np.arange(vol.GetNdaughters()):
-        fnode = vol.GetNode(int(i))
-        # NOTE, this is defined in geometry plugin, fiber_core is the wanted sensitive detector
+    for i in np.arange(gnode.GetNdaughters()):
+        fnode = gnode.GetDaughter(int(i))
+        # NOTE, this is defined in geometry plugin, fiber_core is the only wanted sensitive detector
         if 'fiber_core' not in fnode.GetName():
             continue
         fpos = np.array([0., 0., 0.])
@@ -52,7 +64,7 @@ def get_fibers(detelem, id_desc, id_conv, id_dict, grid=1):
         fnode.LocalToMaster(np.array([0., 0., 0.]), fpos)
         gnode.LocalToMaster(fpos, gpos)
         # the two method below are equivalent
-        global_trans.LocalToMaster(gpos, pos)
+        gtrans.LocalToMaster(gpos, pos)
         # detelem.nominal().localToWorld(gpos, pos)
         """ a test with converter
         if i < 50:
@@ -66,9 +78,9 @@ def get_fibers(detelem, id_desc, id_conv, id_dict, grid=1):
                   fpos,
                   gpos)
         """
-        fibers.append(pos)
+        fibers.append(np.hstack([pos, fr]))
 
-    return fibers, grpos
+    return np.array(fibers), grpos
 
 
 if __name__ == '__main__':
@@ -88,8 +100,8 @@ if __name__ == '__main__':
             help='Readout class for the detector.'
             )
     parser.add_argument(
-            '--vpath', default='stave1/layer1/slice1/grid3',
-            hlelp='Path down to a grid volume to be centered at the plot'
+            '--grid-path', default='module:1,layer:6,slice:1,grid:3',
+            help='Path down to a grid volume to be centered at the plot, with the format \"field1:i1,field2:i2,...\"',
             )
     parser.add_argument(
             '--outdir',
@@ -107,7 +119,7 @@ if __name__ == '__main__':
     try:
         det = det.child(args.detector)
     except Exception:
-        print('Failed to find detector {} from \"{}\"'.format(args.detector, args.compact)
+        print('Failed to find detector {} from \"{}\"'.format(args.detector, args.compact))
         print('Available detectors are listed below:')
         for n, d in desc.world().children:
             print(' --- detector: {}'.format(n))
@@ -117,48 +129,44 @@ if __name__ == '__main__':
     vman = dd4hep.VolumeManager(det, desc.readout(args.readout))
     converter = DDRec.CellIDPositionConverter(desc)
 
-    # manager instances
-    readout = desc.readout(args.readout)
-    idspec = readout.idSpec()
-
-    # build a dictionary for the readout ids
-    mvol_id = det.volumeID()
-    id_dict = OrderedDict()
-    for idstr in idspec.decoder().valueString(mvol_id).split(','):
-        field, fid = idstr.split(':')
-        id_dict[field] = int(fid)
-
-    # get radius of the fiber
-    id_dict.update({'grid': args.center_grid, 'fiber': 1})
-    fid = idspec.encode(dict_to_cpp_vec(id_dict))
-    # need to do unit conversion from mm to cm (might from ROOT/DD4Hep difference)
-    fr = converter.cellDimensions(fid)[0]/2./10.
+    fields = OrderedDict([['system', det.id()]] + [v.split(':') for v in args.grid_path.split(',')])
+    layer = int(fields.get('layer'))
+    grid = int(fields.get('grid'))
+    # add adjacent layers and grids, put the central one at the first
+    id_dicts = []
+    for dl in [0, 1, 2, -1, -2]:
+        for dg in [0, 1, 2, -1, -2]:
+            if layer + dl < 1 or grid + dg < 1:
+                continue
+            new_dict = fields.copy()
+            new_dict.update({'layer': layer + dl, 'grid': grid + dg})
+            id_dicts.append(new_dict)
 
     # plot fibers in the grid
     fig, ax = plt.subplots(figsize=(12, 12), dpi=160)
-    # default color cycles
+    # default color cycle
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    for i, ids in enumerate(id_dicts):
+        c = colors[i % len(colors)]
+        fibers, gr_pos = get_grid_fibers(det, vman, converter, ids)
+        if fibers is None:
+            print('ignored {} because the corresponding volume is not found.'.format(ids))
+            continue
 
-    # plot the center grid and adjacent grid
-    grids = [
-        (g, colors[i]) for i, g in enumerate([args.center_grid, args.center_grid - 1, args.center_grid + 1]) if g > 0
-        ]
-    for g, c in grids:
-        # get position of fibers
-        fibers, grid_pos = get_fibers(det, idspec, converter, id_dict, g)
         patches = []
-        for fpos in fibers:
-            patches.append(Circle((fpos[0], fpos[1]), fr))
+        for fi in fibers:
+            patches.append(Circle((fi[0], fi[1]), fi[3]))
         p = PatchCollection(patches, alpha=0.4, facecolors=(c,), edgecolors=('k',))
-        ax.plot([grid_pos.X()], [grid_pos.Y()], marker='P', mfc=c, mec='k', ms=9, label='grid {} center'.format(g))
+        ax.plot(gr_pos[0], gr_pos[1], marker='P', mfc=c, mec='k', ms=9, label='grid {}'.format(ids['grid']))
         ax.add_collection(p)
-        if g == args.center_grid:
-            ax.set_xlim(grid_pos.X()- 3., grid_pos.X() + 3.)
-            ax.set_ylim(grid_pos.Y() - 3., grid_pos.Y() + 3.)
-    ax.legend(fontsize=22)
+        # center at the first entry
+        if i == 0:
+            ax.set_xlim(gr_pos[0] - 4., gr_pos[0] + 4.)
+            ax.set_ylim(gr_pos[1] - 4., gr_pos[1] + 4.)
+
+    # ax.legend(fontsize=22)
     ax.tick_params(labelsize=20, direction='in')
     ax.set_xlabel('Global X (mm)', fontsize=22)
     ax.set_ylabel('Global Y (mm)', fontsize=22)
-    ax.set_title('{} around grid {}'.format(args.dpath, args.center_grid), fontsize=22)
-
+    ax.set_title('Centered at {}'.format('/'.join(['{}{}'.format(k, v) for k, v in fields.items()])), fontsize=22)
     fig.savefig(os.path.join(args.outdir, 'grid_fibers.png'))
