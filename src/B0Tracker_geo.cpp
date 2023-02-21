b1ecb5cf68951cdb7c45b538e53a8c014c33e26d
Wouter Deconinck <wdconinc@gmail.com>
2023-02-21 15:47:49 -0600

    Update src/B0Tracker_geo.cpp

--- src/B0Tracker_geo.cpp
+++ src/B0Tracker_geo.cpp
@@ -217,7 +217,6 @@ static Ref_t create_B0Tracker(Detector& description, xml_h e, SensitiveDetector 
           PlacedVolume sens_pv = sensVols[ic];
           DetElement   comp_elt(module, sens_pv.volume().name(), mod_num);
           comp_elt.setPlacement(sens_pv);
-          // std::cout << " adding ACTS extension" << "\n";
           auto &comp_elt_params = DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(comp_elt);
           comp_elt_params.set<std::string>("axis_definitions", "XZY");
           volSurfaceList(comp_elt)->push_back(volplane_surfaces[m_nam][ic]);
