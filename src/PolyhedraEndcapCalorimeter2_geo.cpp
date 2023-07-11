8884710289113c9900c335f7d312bac0f7d7fce7
Alexandr Prozorov <alexandr.a.prozorov@gmail.com>
2023-07-11 06:05:09 -0400

    changed layering to 5 layers with steel235 and 5x stainless steel + last polysterene layer

--- src/PolyhedraEndcapCalorimeter2_geo.cpp
+++ src/PolyhedraEndcapCalorimeter2_geo.cpp
@@ -5,57 +5,54 @@ using namespace std;
 using namespace dd4hep;
 using namespace dd4hep::detail;
 
-// .q
 const unsigned nHcalSectors          = 12;
 const double   starEmcEtaBins[13]    = {2.0,    1.9008, 1.8065, 1.7168, 1.6317, 1.5507, 1.4738,
                                         1.4007, 1.3312, 1.2651, 1.2023, 1.1427, 1.086};
-const double   outerHcalEtaBins[3]   = {1.42823, 1.3672, 1.30824};
-const double   innerHcalEtaBins[7]   = {3.10639, 2.97419, 2.84764, 2.72653, 2.61067, 2.49986, 2.3939};
+const double   outerHcalEtaBins[3]   = {1.42718, 1.36617, 1.30723};
+const double   innerHcalEtaBins[7]   = {3.10522, 2.97302, 2.84647, 2.72537, 2.60951, 2.4987, 2.39275};
 const double   starEmcZPositions[24] = {270.19,  271.695, 273.15,  274.555, 275.96,  277.365, 282.363, 283.768,
                                         285.173, 286.578, 287.983, 289.388, 290.793, 292.198, 293.603, 295.008,
                                         296.413, 297.818, 299.223, 300.628, 302.033, 303.438, 304.843, 306.158};
-const int      nNewLayers            = 11;
-const int      tileMap[nNewLayers]   = {0, 2, 4, 5, 6, 8, 10, 12, 15, 17, 19};
+const int      tileMap[11]           = {0, 2, 4, 5, 6, 8, 10, 12, 15, 17, 19};
 double         getR(const double& z, const double& eta) { return z / (sinh(eta)); }
 
 void buildSubsector(Detector& description, xml_h e, SensitiveDetector sens, vector<PlacedVolume>& sensitives,
-                    const int& layerNumber, const xml_comp_t x_slice, Volume& sectorVol, const int& nSubSectors,
-                    const int& nEtaBins, const double* etaBins, const double& oldEmcGap = 0, bool isStarEmc = false)
+                    const int& layerNumber, const double& globalZ, const xml_comp_t x_slice, Volume& sectorVol,
+                    const int& nSubSectors, const int& nEtaBins, const double* etaBins, const double& oldEmcGap = 0,
+                    bool isStarEmc = false)
 {
   xml_det_t x_det = e;
-  xml_dim_t dim   = x_det.dimensions();
   Material  air   = description.air();
-  double    zmin  = dim.zmin();
   Layering  layering(x_det);
-  double    layer_thickness = layering.layer(layerNumber - 1)->thickness();
-  double    s_thick         = x_slice.thickness();
-  
+  double    slice_thickness = x_slice.thickness();
+
   double dphiSector   = 2. * M_PI / (nHcalSectors);      // 30  deg
   double dphi         = dphiSector / nSubSectors;        // 6 deg
   int    oldTileLayer = tileMap[layerNumber - 1];        // layer of the old tile corresponding to the current layer
   double zOldPos      = starEmcZPositions[oldTileLayer]; // center z position of the old tile
-  double zPos         = zmin + (layerNumber-1)*layer_thickness + s_thick / 2;
-
+  double zPos         = globalZ;
   double rmax = isStarEmc ? getR(zOldPos, starEmcEtaBins[nEtaBins])
                           : getR(zPos, etaBins[nEtaBins]) - oldEmcGap; // closest tile distance to the beam pipe
   double rmin = isStarEmc ? getR(zOldPos, starEmcEtaBins[0])
-                          : getR(zPos, etaBins[0]) -oldEmcGap ;        // furthest tile distance to the beam pipe
+                          : getR(zPos, etaBins[0]) - oldEmcGap;        // furthest tile distance to the beam pipe
 
-  Volume subSectorVol("subsector", Tube(rmin, rmax, s_thick / 2, 0, dphi), air);
+  Volume subSectorVol("subsector", Tube(rmin, rmax, slice_thickness / 2, 0, dphi), air);
 
   for (int iEtaBin = 0; iEtaBin < nEtaBins; iEtaBin++) {
     // recreate tile size for the given layer:
-    double rBottom = isStarEmc ? getR(zOldPos, starEmcEtaBins[iEtaBin])
-                               : getR(zPos, etaBins[iEtaBin]) -oldEmcGap ;     // current tile closest distance to the beam pipe
-    double rTop    = isStarEmc ? getR(zOldPos, starEmcEtaBins[iEtaBin + 1])
-                               : getR(zPos, etaBins[iEtaBin + 1]) -oldEmcGap ; // current tile closest distance to the beam pipe
+    double rBottom = isStarEmc
+                         ? getR(zOldPos, starEmcEtaBins[iEtaBin])
+                         : getR(zPos, etaBins[iEtaBin]) - oldEmcGap; // current tile closest distance to the beam pipe
+    double rTop    = isStarEmc
+                         ? getR(zOldPos, starEmcEtaBins[iEtaBin + 1])
+                         : getR(zPos, etaBins[iEtaBin + 1]) - oldEmcGap; // current tile closest distance to the beam pipe
     // cosine theorem for the angle between the two radii:
     //(2 dx1)^2 = r^2+r^2-2*r*r*cos(phi) = 2r^2(1-cos(phi));
     //   dx1=r*sqrt(2(1-cos(phi)))/2;
     double    dx1 = rBottom * sqrt(2 * (1 - cos(dphi))) / 2;
     double    dx2 = rTop * sqrt(2 * (1 - cos(dphi))) / 2;
     double    dz  = (rTop - rBottom) / 2;
-    double    dy  = s_thick / 2;
+    double    dy  = slice_thickness / 2;
     Trapezoid tile(dx1, dx2, dy, dy, dz);
     Material  s_mat = description.material(x_slice.materialStr());
     string    tileName =
@@ -67,12 +64,9 @@ void buildSubsector(Detector& description, xml_h e, SensitiveDetector sens, vect
         tile_vol, Transform3D(RotationZYX(0, M_PI / 2 + dphi / 2, M_PI / 2),
                               Position(distance * cos(dphi / 2), distance * sin(dphi / 2), 0)));
     tile_phys.addPhysVolID("etabin", iEtaBin);
-
-    if (x_slice.isSensitive()) {
-      sens.setType("calorimeter");
-      tile_vol.setSensitiveDetector(sens);
-      sensitives.push_back(tile_phys);
-    }
+    sens.setType("calorimeter");
+    tile_vol.setSensitiveDetector(sens);
+    sensitives.push_back(tile_phys);
   }
 
   for (int iSubSector = 0; iSubSector < nSubSectors; iSubSector++) {
@@ -103,89 +97,83 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
   double layerZ = -totalThickness / 2;
   endcapVol.setAttributes(description, x_det.regionStr(), x_det.limitsStr(), x_det.visStr());
 
-  xml_coll_t xf(x_det, _U(layer));
-  xml_comp_t x_layer_first = xf;
-  int        l_repeat      = x_layer_first.repeat();
+  int    iLayer  = 0;
+  double globalZ = zmin;
+  // Looping through all the different layer sections
+  for (xml_coll_t xc(x_det, _U(layer)); xc; ++xc) {
+    xml_comp_t x_layer         = xc;
+    int        repeat          = x_layer.repeat();
+    double     layer_thickness = x_layer.thickness();
 
-  if (l_repeat <= 0) {
-    throw std::runtime_error(x_det.nameStr() + "> Invalid repeat value");
-  }
-  for (int iLayer = 1; iLayer <= l_repeat; ++iLayer) {
-
-    for (xml_coll_t xc(x_det, _U(layer)); xc; ++xc) {
-      xml_comp_t           x_layer = xc;
-      double               l_thick = layering.layer(iLayer - 1)->thickness();
-      string               l_name  = _toString(iLayer - 1, "layer%d");
-      Volume               l_vol(l_name, PolyhedraRegular(numsides, rmin, rmax, l_thick), air);
+    // Looping through the number of repeated layers in each section
+    for (int i = 0; i < repeat; i++) {
+      iLayer++;
+      string               l_name = _toString(iLayer - 1, "layer%d");
+      Volume               layer_volume("layer", PolyhedraRegular(numsides, rmin, rmax, layer_thickness), air);
       vector<PlacedVolume> sensitives;
-      int                  s_num    = 1;
-      double               sliceZ   = -l_thick / 2;
-      double               disksGap = getAttrOrDefault(x_layer, _Unicode(disksGap), 1 * cm);
+      int                  slice_number = 1;
+      double               sliceZ       = -layer_thickness / 2;
+      double               disksGap     = getAttrOrDefault(x_layer, _Unicode(disksGap), 1 * cm);
 
       for (xml_coll_t xs(x_layer, _U(slice)); xs; ++xs) {
-        xml_comp_t x_slice = xs;
-        string     s_name  = _toString(s_num, "slice%d");
-        double     s_thick = x_slice.thickness();
-        Material   s_mat   = description.material(x_slice.materialStr());
-        sliceZ += s_thick / 2;
+        xml_comp_t x_slice         = xs;
+        string     slice_name      = _toString(slice_number, "slice%d");
+        double     slice_thickness = x_slice.thickness();
+        globalZ += slice_thickness / 2;
 
-        Volume slice("slice", Tube(rmin, rmax, s_thick / 2), air);
+        Material s_mat = description.material(x_slice.materialStr());
+        sliceZ += slice_thickness / 2;
 
-        if (x_slice.visStr() == "HcalAbsorberVis") {
-          Volume absorber("absorber", Tube(rmin, rmax, s_thick / 2, M_PI / 2, M_PI * 3 / 2), s_mat);
-          absorber.setVisAttributes(description.visAttributes(x_slice.visStr()));
-          PlacedVolume s_phv1 = slice.placeVolume(absorber, Position(-disksGap / 2, 0, 0));
-          s_phv1.addPhysVolID("absorberDisk", 0);
-          PlacedVolume s_phv2 =
-              slice.placeVolume(absorber, Transform3D(RotationZYX(M_PI, 0, 0), Position(+disksGap / 2, 0, 0)));
-          s_phv2.addPhysVolID("absorberDisk", 1);
+        Volume slice_volume("slice", Tube(rmin, rmax, slice_thickness / 2), air);
 
-          PlacedVolume s_phv = l_vol.placeVolume(slice, Position(0, 0, sliceZ));
-          s_phv.addPhysVolID("slice", s_num);
-        } 
-     
-        
-        
-        
-        else {
-          ConeSegment sectorShape(s_thick / 2, rmin, rmax, rmin, rmax, 0, 2 * M_PI / (nHcalSectors));
+        if (x_slice.isSensitive()) {
+          ConeSegment sectorShape(slice_thickness / 2, rmin, rmax, rmin, rmax, 0, 2 * M_PI / (nHcalSectors));
           Volume      sectorVol{"sector", sectorShape, air};
           // void buildSubsector(Detector& description, xml_h e, SensitiveDetector sens, vector<PlacedVolume>&
           // sensitives,
-          //     const double& s_thick, const int& layerNumber, const xml_comp_t x_slice, Volume& sectorVol,
+          //     const double& slice_thickness, const int& layerNumber, const xml_comp_t x_slice, Volume& sectorVol,
           //     const int& nSubSectors, const int& nEtaBins, const double* etaBins, bool isStarEmc = false)
-
-          buildSubsector(description, e, sens, sensitives, iLayer, x_slice, sectorVol, 10, 2, outerHcalEtaBins, -2.18);
-
-          buildSubsector(description, e, sens, sensitives, iLayer, x_slice, sectorVol, 5, 12, starEmcEtaBins, 0.,
-                         true); // star Emc implementation for new Z position with 12 eta bins
-          buildSubsector(description, e, sens, sensitives, iLayer, x_slice, sectorVol, 5, 3, &innerHcalEtaBins[3],
-                         2.18); // 3 eta bins of inner sector with the same substructure as Star Emc with
-                                // // 6 (30/5)degree tiles
-          buildSubsector(description, e, sens, sensitives, iLayer, x_slice, sectorVol, 3, 2, &innerHcalEtaBins[1],
-                         2.18); // 2 eta bins of inner sector with 10 (30/3) degree tiles
-          buildSubsector(description, e, sens, sensitives, iLayer, x_slice, sectorVol, 2, 1, &innerHcalEtaBins[0],
-                         2.18); // 1 eta bins of innermost sector with 15 (30/2) degree tiles
+          buildSubsector(description, e, sens, sensitives, iLayer, globalZ, x_slice, sectorVol, 10, 2, outerHcalEtaBins,
+                         -2.18);                      // 2 eta bins of outer sector with 3 (30/10) degree tiles
+          buildSubsector(description, e, sens, sensitives, iLayer, globalZ, x_slice, sectorVol, 5, 12, starEmcEtaBins,
+                         0., true);                   // star Emc implementation for new Z position with 12 eta bins
+          buildSubsector(description, e, sens, sensitives, iLayer, globalZ, x_slice, sectorVol, 5, 3,
+                         &innerHcalEtaBins[3], 2.18); // 3 eta bins of  Star Emc sector with 6 (30/5)degree tiles
+          buildSubsector(description, e, sens, sensitives, iLayer, globalZ, x_slice, sectorVol, 3, 2,
+                         &innerHcalEtaBins[1], 2.18); // 2 eta bins of inner sector with 10 (30/3) degree tiles
+          buildSubsector(description, e, sens, sensitives, iLayer, globalZ, x_slice, sectorVol, 2, 1,
+                         &innerHcalEtaBins[0], 2.18); // 1 eta bins of innermost sector with 15 (30/2) degree tiles
 
           for (unsigned iSector = 0; iSector < nHcalSectors; iSector++) {
-            PlacedVolume sectorVol_phys = slice.placeVolume(
+            PlacedVolume sectorVol_phys = slice_volume.placeVolume(
                 sectorVol, Transform3D(RotationZYX(M_PI / 2 + iSector * 2 * M_PI / (nHcalSectors), 0, 0),
                                        Position(((iSector + 1) > nHcalSectors / 2 ? disksGap : -disksGap), 0, 0)));
             sectorVol_phys.addPhysVolID("sector", iSector);
           }
-          PlacedVolume s_phv = l_vol.placeVolume(slice, Position(0, 0, sliceZ));
-          s_phv.addPhysVolID("slice", s_num);
+          PlacedVolume s_phv = layer_volume.placeVolume(slice_volume, Position(0, 0, sliceZ));
+          s_phv.addPhysVolID("slice", slice_number);
+        } else {
+          Volume halfdisk("halfdisk", Tube(rmin, rmax, slice_thickness / 2, M_PI / 2, M_PI * 3 / 2), s_mat);
+          halfdisk.setVisAttributes(description.visAttributes(x_slice.visStr()));
+          PlacedVolume s_phv1 = slice_volume.placeVolume(halfdisk, Position(-disksGap / 2, 0, 0));
+          s_phv1.addPhysVolID("halfdisk", 0);
+          PlacedVolume s_phv2 =
+              slice_volume.placeVolume(halfdisk, Transform3D(RotationZYX(M_PI, 0, 0), Position(+disksGap / 2, 0, 0)));
+          s_phv2.addPhysVolID("halfdisk", 1);
+          PlacedVolume s_phv = layer_volume.placeVolume(slice_volume, Position(0, 0, sliceZ));
+          s_phv.addPhysVolID("slice", slice_number);
         }
-        sliceZ += s_thick / 2;
-        s_num++;
+        globalZ += slice_thickness / 2;
+        sliceZ += slice_thickness / 2;
+        slice_number++;
       }
 
-      l_vol.setVisAttributes(description.visAttributes(x_layer.visStr()));
+      layer_volume.setVisAttributes(description.visAttributes(x_layer.visStr()));
 
       string phys_lay = Form("layer%i", iLayer);
-      layerZ += l_thick / 2;
+      layerZ += layer_thickness / 2;
       DetElement   layer_elt(endcap, phys_lay, iLayer);
-      PlacedVolume pv = endcapVol.placeVolume(l_vol, Position(0, 0, layerZ));
+      PlacedVolume pv = endcapVol.placeVolume(layer_volume, Position(0, 0, layerZ));
       pv.addPhysVolID("layer", iLayer);
       layer_elt.setPlacement(pv);
       for (size_t ic = 0; ic < sensitives.size(); ++ic) {
@@ -193,7 +181,7 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
         DetElement   comp_elt(layer_elt, sens_pv.volume().name(), iLayer);
         comp_elt.setPlacement(sens_pv);
       }
-      layerZ += l_thick / 2;
+      layerZ += layer_thickness / 2;
     }
   }
   double       z_pos = zmin + totalThickness / 2;
