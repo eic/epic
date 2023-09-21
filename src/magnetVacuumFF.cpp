bd329eadd3b94efd27dae0e2de7f1c81f5c5daeb
ajentsch <alexmichaeljentsch@gmail.com>
2023-09-21 11:12:20 -0400

    remove dependencies on ROOT::TMath for trig and constants

--- src/magnetVacuumFF.cpp
+++ src/magnetVacuumFF.cpp
@@ -4,7 +4,6 @@
 
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/Printout.h"
-#include "TMath.h"
 #include <XML/Helper.h>
 
 using namespace std;
@@ -31,7 +30,7 @@ double getRotatedX(double z, double x, double angle);
 static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens */)
 {
 
-        using namespace ROOT::Math;
+        //using namespace ROOT::Math;
         xml_det_t x_det    = e;
         string    det_name = x_det.nameStr();
         DetElement sdet(det_name, x_det.id());
@@ -66,7 +65,7 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
         int idx = 0;
         for(xml_coll_t c(x_det,_U(element)); c; ++c){
 
-                xml_dim_t pos       = c.child(_U(placement));
+			xml_dim_t pos       = c.child(_U(placement));
             double    pos_x     = pos.x();
             double    pos_y     = pos.y();
             double    pos_z     = pos.z();
@@ -77,19 +76,22 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
             double    app_r     = apperture.r();
 
 
-                radii_magnet[idx]     = app_r; // cm
-                lengths_magnet[idx]   = dim_z; //cm
-                rotation_magnet[idx]  = pos_theta;  // radians
-                x_elem_magnet[idx]    = pos_x*dd4hep::cm;
-                y_elem_magnet[idx]    = pos_y*dd4hep::cm;
-                z_elem_magnet[idx]    = pos_z*dd4hep::cm;
+            radii_magnet[idx]     = app_r; // cm
+            lengths_magnet[idx]   = dim_z; //cm
+            rotation_magnet[idx]  = pos_theta;  // radians
+            x_elem_magnet[idx]    = pos_x*dd4hep::cm;
+            y_elem_magnet[idx]    = pos_y*dd4hep::cm;
+            z_elem_magnet[idx]    = pos_z*dd4hep::cm;
 
-                idx++;
+            idx++;
         }
 
         //-------------------------------------------
-        //override numbers for the first element -->
-        //doesn't use the actual B0pf geometry!!!
+        // override numbers for the first element -->
+        // doesn't use the actual B0pf geometry!!!
+		// -->it's based on the B0 beam pipe
+		// this needs to be fixed later to read-in 
+		// that beam pipe geometry
         //-------------------------------------------
 
         radii_magnet[0]     = 2.9;     // cm
@@ -154,10 +156,10 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
         for(int i = 1; i < numMagnets; i++){
 
-                angle_elem_gap[i-1] = (x_beg[i] - x_end[i-1])/(z_beg[i] - z_end[i-1]);
-                length_gap[i-1] = TMath::Sqrt(TMath::Power(z_beg[i] - z_end[i-1], 2) + TMath::Power(x_beg[i] - x_end[i-1], 2));
-                z_gap[i-1] = z_end[i-1] + 0.5*length_gap[i-1]*TMath::Cos(angle_elem_gap[i-1]);
-                x_gap[i-1] = x_end[i-1] + 0.5*length_gap[i-1]*TMath::Sin(angle_elem_gap[i-1]);
+            angle_elem_gap[i-1] = (x_beg[i] - x_end[i-1])/(z_beg[i] - z_end[i-1]);
+            length_gap[i-1] = sqrt(pow(z_beg[i] - z_end[i-1], 2) + pow(x_beg[i] - x_end[i-1], 2));
+            z_gap[i-1] = z_end[i-1] + 0.5*length_gap[i-1]*cos(angle_elem_gap[i-1]);
+            x_gap[i-1] = x_end[i-1] + 0.5*length_gap[i-1]*sin(angle_elem_gap[i-1]);
 
         }
 
@@ -174,16 +176,16 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
         for(int gapIdx = 0; gapIdx < numGaps; gapIdx++){
 
-                inRadius[gapIdx]    = 0.0;
-                outRadius[gapIdx]   = radii_magnet[gapIdx+1];
-                phi_initial[gapIdx] = 0.0;
-                phi_final[gapIdx]   = TMath::TwoPi();
-                nxLow[gapIdx]       = -(length_gap[gapIdx]/2.0)*TMath::Sin(rotation_magnet[gapIdx]-angle_elem_gap[gapIdx]);
-                nyLow[gapIdx]       = 0.0;
-                nzLow[gapIdx]       = -(length_gap[gapIdx]/2.0)*TMath::Cos(rotation_magnet[gapIdx]-angle_elem_gap[gapIdx]);
-                nxHigh[gapIdx]      = (length_gap[gapIdx]/2.0)*TMath::Sin(rotation_magnet[gapIdx+1]-angle_elem_gap[gapIdx]);
-                nyHigh[gapIdx]      = 0.0;
-                nzHigh[gapIdx]      = (length_gap[gapIdx]/2.0)*TMath::Cos(rotation_magnet[gapIdx+1]-angle_elem_gap[gapIdx]);
+            inRadius[gapIdx]    = 0.0;
+            outRadius[gapIdx]   = radii_magnet[gapIdx+1];
+            phi_initial[gapIdx] = 0.0;
+            phi_final[gapIdx]   = 2*M_PI;
+            nxLow[gapIdx]       = -(length_gap[gapIdx]/2.0)*sin(rotation_magnet[gapIdx]-angle_elem_gap[gapIdx]);
+            nyLow[gapIdx]       = 0.0;
+            nzLow[gapIdx]       = -(length_gap[gapIdx]/2.0)*cos(rotation_magnet[gapIdx]-angle_elem_gap[gapIdx]);
+            nxHigh[gapIdx]      = (length_gap[gapIdx]/2.0)*sin(rotation_magnet[gapIdx+1]-angle_elem_gap[gapIdx]);
+            nyHigh[gapIdx]      = 0.0;
+            nzHigh[gapIdx]      = (length_gap[gapIdx]/2.0)*cos(rotation_magnet[gapIdx+1]-angle_elem_gap[gapIdx]);
 
         }
 
@@ -193,15 +195,14 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
         for(int pieceIdx = 0; pieceIdx < numMagnets; pieceIdx++){
 
-                std::string piece_name      = Form("MagnetVacuum%d", pieceIdx);
+            std::string piece_name      = Form("MagnetVacuum%d", pieceIdx);
 
-                Tube magnetPiece(piece_name, 0.0, radii_magnet[pieceIdx], lengths_magnet[pieceIdx]/2);
-                Volume vpiece(piece_name, magnetPiece, m_Vac);
-                sdet.setAttributes(det, vpiece, x_det.regionStr(), x_det.limitsStr(), vis_name);
+            Tube magnetPiece(piece_name, 0.0, radii_magnet[pieceIdx], lengths_magnet[pieceIdx]/2);
+            Volume vpiece(piece_name, magnetPiece, m_Vac);
+            sdet.setAttributes(det, vpiece, x_det.regionStr(), x_det.limitsStr(), vis_name);
 
-            auto pv = assembly.placeVolume(vpiece,
-                                                                       Transform3D(RotationY(rotation_magnet[pieceIdx]),
-                                                                       Position(x_elem_magnet[pieceIdx], y_elem_magnet[pieceIdx], z_elem_magnet[pieceIdx])));
+            auto pv = assembly.placeVolume(vpiece, Transform3D(RotationY(rotation_magnet[pieceIdx]),
+                                                   Position(x_elem_magnet[pieceIdx], y_elem_magnet[pieceIdx], z_elem_magnet[pieceIdx])));
             pv.addPhysVolID("sector", 1);
             detectorElement[pieceIdx] = new DetElement(sdet, Form("sector%d_de", pieceIdx), 1);
             detectorElement[pieceIdx]->setPlacement(pv);
@@ -214,20 +215,19 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
         for(int pieceIdx = numMagnets; pieceIdx < numGaps + numMagnets; pieceIdx++){
 
-                int correctIdx = pieceIdx-numMagnets;
+            int correctIdx = pieceIdx-numMagnets;
 
-                std::string piece_name  = Form("GapVacuum%d", correctIdx);
+            std::string piece_name  = Form("GapVacuum%d", correctIdx);
 
-        CutTube gapPiece(piece_name, inRadius[correctIdx], outRadius[correctIdx], length_gap[correctIdx]/2, phi_initial[correctIdx], phi_final[correctIdx],
-                                                                                nxLow[correctIdx], nyLow[correctIdx], nzLow[correctIdx], nxHigh[correctIdx], nyHigh[correctIdx], nzHigh[correctIdx]);
+       	 	CutTube gapPiece(piece_name, inRadius[correctIdx], outRadius[correctIdx], length_gap[correctIdx]/2, phi_initial[correctIdx], phi_final[correctIdx],
+                                               nxLow[correctIdx], nyLow[correctIdx], nzLow[correctIdx], nxHigh[correctIdx], nyHigh[correctIdx], nzHigh[correctIdx]);
 
-                Volume vpiece(piece_name, gapPiece, m_Vac);
-        sdet.setAttributes(det, vpiece, x_det.regionStr(), x_det.limitsStr(), vis_name);
+            Volume vpiece(piece_name, gapPiece, m_Vac);
+        	sdet.setAttributes(det, vpiece, x_det.regionStr(), x_det.limitsStr(), vis_name);
 
-                auto pv = assembly.placeVolume(vpiece,
-                                                                       Transform3D(RotationY(angle_elem_gap[correctIdx]),
-                                                                       Position(x_gap[correctIdx], 0.0, z_gap[correctIdx])));
-            pv.addPhysVolID("sector", 1);
+			auto pv = assembly.placeVolume(vpiece, Transform3D(RotationY(angle_elem_gap[correctIdx]),
+                                                   Position(x_gap[correctIdx], 0.0, z_gap[correctIdx])));
+			pv.addPhysVolID("sector", 1);
             detectorElement[pieceIdx] = new DetElement(sdet, Form("sector%d_de", pieceIdx), 1);
             detectorElement[pieceIdx]->setPlacement(pv);
 
@@ -239,21 +239,21 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
         if(makeIP_B0pfVacuum){
 
-                double specialGapLength = TMath::Sqrt(TMath::Power(z_beg[0] - endOfCentralBeamPipe_z, 2) + TMath::Power(x_beg[0] - endOfCentralBeamPipe_x, 2)) - 0.1;
-                double specialGap_z = 0.5*specialGapLength*TMath::Cos(crossingAngle) + endOfCentralBeamPipe_z;
-                double specialGap_x = 0.5*specialGapLength*TMath::Sin(crossingAngle) + endOfCentralBeamPipe_x;
+        	double specialGapLength = sqrt(pow(z_beg[0] - endOfCentralBeamPipe_z, 2) + pow(x_beg[0] - endOfCentralBeamPipe_x, 2)) - 0.1;
+			double specialGap_z = 0.5*specialGapLength*cos(crossingAngle) + endOfCentralBeamPipe_z;
+			double specialGap_x = 0.5*specialGapLength*sin(crossingAngle) + endOfCentralBeamPipe_x;
 
-                std::string piece_name  = Form("GapVacuum%d", numGaps + numMagnets);
+			std::string piece_name  = Form("GapVacuum%d", numGaps + numMagnets);
 
-                Cone specialGap(piece_name, specialGapLength/2, 0.0, vacuumDiameterEntrance/2, 0.0, vacuumDiameterExit/2 );
+			Cone specialGap(piece_name, specialGapLength/2, 0.0, vacuumDiameterEntrance/2, 0.0, vacuumDiameterExit/2 );
 
-                Volume specialGap_v(piece_name, specialGap, m_Vac);
-                sdet.setAttributes(det, specialGap_v, x_det.regionStr(), x_det.limitsStr(), vis_name);
+			Volume specialGap_v(piece_name, specialGap, m_Vac);
+			sdet.setAttributes(det, specialGap_v, x_det.regionStr(), x_det.limitsStr(), vis_name);
 
-                auto pv = assembly.placeVolume(specialGap_v, Transform3D(RotationY(crossingAngle), Position(specialGap_x, 0.0, specialGap_z)));
-        pv.addPhysVolID("sector", 1);
-        detectorElement[numGaps + numMagnets] = new DetElement(sdet, Form("sector%d_de", numGaps + numMagnets), 1);
-                detectorElement[numGaps + numMagnets]->setPlacement(pv);
+			auto pv = assembly.placeVolume(specialGap_v, Transform3D(RotationY(crossingAngle), Position(specialGap_x, 0.0, specialGap_z)));
+			pv.addPhysVolID("sector", 1);
+			detectorElement[numGaps + numMagnets] = new DetElement(sdet, Form("sector%d_de", numGaps + numMagnets), 1);
+			detectorElement[numGaps + numMagnets]->setPlacement(pv);
 
         }
 
@@ -268,12 +268,12 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
 double getRotatedZ(double z, double x, double angle){
 
-        return z*TMath::Cos(angle) - x*TMath::Sin(angle);
+        return z*cos(angle) - x*sin(angle);
 }
 
 double getRotatedX(double z, double x, double angle){
 
-        return z*TMath::Sin(angle) + x*TMath::Cos(angle);
+        return z*sin(angle) + x*cos(angle);
 }
 
 DECLARE_DETELEMENT(magnetElementInnerVacuum, create_detector)
