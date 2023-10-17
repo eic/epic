4f8fec7ee77fca3024ca75b14428de4a6be0084a
billlee <wenliang.billlee@gmail.com>
2023-10-17 10:42:17 -0400

    Oct 17

--- src/PFRICH_geo_v1.cpp
+++ src/PFRICH_geo_v1.cpp
@@ -51,7 +51,100 @@ static Ref_t createDetector(Detector& description, xml_h e, Ref_t)
 //  sens.setType("tracker");
   description.invisible(); 
 
-  cout << "aaaaaaaaaaaaaaaaaaaaaaaa " << endl;
+  xml::DetElement       detElem = e;
+  xml::Component        dims    = detElem.dimensions();
+
+  double vessel_radius   =  dims.attr<double>(_Unicode(rmax));;
+
+  ///*--------------------------------------------------*/
+  /// Porting in GDML volume
+ 
+  xml_dim_t  x_gdml(x_det.child(_U(gdmlFile)));
+  xml_dim_t  x_par(x_det.child(_U(parent)));
+
+ string     name         = x_det.nameStr();
+  string     gdml         = x_gdml.attr<string>(_U(ref));
+  string     par_nam        = x_par.nameStr();
+  DetElement det_parent   = description.detector(par_nam);
+
+  TGDMLParse parser;
+  if (!gdml.empty() && gdml[0] == '/') {
+    TUri uri(gdml.c_str());
+    gdml = uri.GetRelativePart();
+  } else {
+    string path = xml::DocumentHandler::system_path(e, gdml);
+    TUri   uri(path.c_str());
+    gdml = uri.GetRelativePart();
+  }
+
+  if (!det_parent.isValid()) {
+    except(name, "+++ Cannot access detector parent: %s", par_nam.c_str());
+  }
+
+//  DetElement sdet(name, id);
+  Volume     volume = parser.GDMLReadFile(gdml.c_str());
+  if (!volume.isValid()) {
+    except("ROOTGDMLParse", "+++ Failed to parse GDML file:%s", gdml.c_str());
+  }
+  volume.import(); // We require the extensions in dd4hep.
+  printout(INFO, "ROOTGDMLParse", "+++ Attach GDML volume %s", volume.name());
+  Volume       mother = det_parent.volume();
+  PlacedVolume pv;
+
+  string     gdml_physvol = dd4hep::getAttrOrDefault<string>(x_gdml, _Unicode(physvol), "");
+
+  if (!gdml_physvol.empty()) {
+    PlacedVolume node = volume->FindNode(gdml_physvol.c_str());
+    if (!node.isValid()) {
+      printout(ERROR, "ROOTGDMLParse", "+++ Invalid gdml placed volume %s", gdml_physvol.c_str());
+      printout(ERROR, "ROOTGDMLParse", "+++ Valid top-level nodes are:");
+      volume->PrintNodes();
+      except("ROOTGDMLParse", "+++ Failed to parse GDML file:%s for node:%s", gdml.c_str(), gdml_physvol.c_str());
+    }
+    volume = node.volume();
+  }
+
+  int        id    = x_det.hasAttr(_U(id)) ? x_det.id() : 0;
+  xml_dim_t  x_pos(x_det.child(_U(position), false));
+  xml_dim_t  x_rot(x_det.child(_U(rotation), false));
+
+  if (x_pos && x_rot) {
+    Rotation3D  rot(RotationZYX(x_rot.z(), x_rot.y(), x_rot.x()));
+    Transform3D transform(rot, Position(x_pos.x(), x_pos.y(), x_pos.z()));
+    pv = mother.placeVolume(volume, transform);
+  } else if (x_rot) {
+    Rotation3D  rot(RotationZYX(x_rot.z(), x_rot.y(), x_rot.x()));
+    Transform3D transform(rot, Position(0, 0, 0));
+    pv = mother.placeVolume(volume, transform);
+  } else if (x_pos) {
+    pv = mother.placeVolume(volume, Position(x_pos.x(), x_pos.y(), x_pos.z()));
+  } else {
+    pv = mother.placeVolume(volume);
+  }
+  volume.setVisAttributes(description, x_det.visStr());
+  volume.setLimitSet(description, x_det.limitsStr());
+  volume.setRegion(description, x_det.regionStr());
+  if (id != 0) {
+    pv.addPhysVolID("system", id);
+  }
+  sdet.setPlacement(pv);
+  return sdet;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  cout << "aaaaaaaaaaaaaaaaaaaaaaaa " << "    " << vessel_radius << endl;
 
   exit(0);
 
