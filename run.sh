855ad19e94236249385be5a183b7fb21e1e6f1ba
Holtzenfich <alexandr.a.prozorov@gmail.com>
2023-10-20 09:02:54 -0400

    added hcal_only design

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 cmake -B build -S . -DCMAKE_INSTALL_PREFIX=install
 cmake --build build -- install -j4
 source install/setup.sh
-dd_web_display --export $DETECTOR_PATH/epic_calorimeters.xml
-checkOverlaps -t 0.0001 -c epic_calorimeters.xml > overlaps.txt
+dd_web_display --export $DETECTOR_PATH/epic_hcal_only.xml
+checkOverlaps -t 0.0001 -c epic_hcal_only.xml > overlaps.txt
