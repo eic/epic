391fd76f5c0ada4ad330a19b9c29fedce9c4b2f5
Kosarzewski <kosarzewski.1@asc.ohio-state.edu>
2023-12-30 11:42:03 +0100

    Corrected positions of flux return, collar and oculus set according to parameter table
    No physical overlaps visible/possible, but still getting errors:
    
    Error in <TGeoVoxelFinder::SortAll>: Wrong bounding box for volume endcapFlux
    Error in <TGeoVoxelFinder::SortAll>: Wrong bounding box for volume FluxEndcapN
    
    Produces fake overlaps indicating some issue in hierarchy, but in principlle works
    
    Signed-off-by: Kosarzewski <kosarzewski.1@asc.ohio-state.edu>

--- src/EndcapFluxReturn_geo.cpp
+++ src/EndcapFluxReturn_geo.cpp
@@ -6,7 +6,7 @@ using namespace dd4hep;
 using namespace dd4hep::detail;
 
 
-static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector sens)
+static Ref_t create_detector(Detector& description, xml_h e, [[maybe_unused]] SensitiveDetector sens)
 {
   xml_det_t x_det    = e;
   xml_dim_t dim      = x_det.dimensions();
@@ -24,31 +24,44 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
   Layering layering(x_det);
   double   totalThickness = layering.totalThickness();
 
-  Volume endcapVol("endcap", Tube(rmin, rmax + 2 * cm, totalThickness), air); // rmax + 2 * cm -to account for disk gap
-  DetElement endcap("endcap", det_id);
+  Volume endcapVol("endcapFlux", Tube(rmin, rmax + 2 * cm, totalThickness), air); // rmax + 2 * cm -to account for disk gap
+  DetElement endcap("endcapFlux", det_id);
 
-  double layerZ = -totalThickness / 2;
   endcapVol.setAttributes(description, x_det.regionStr(), x_det.limitsStr(), x_det.visStr());
 
-  int    iLayer  = 0;
-  double globalZ = zmin;
+  //int    iLayer  = 0;
+  //double globalZ = zmin;
   // Looping through all the different layer sections
   for (xml_coll_t xc(x_det, _U(layer)); xc; ++xc) {
     xml_comp_t x_layer         = xc;
 
+    int     layer_id = x_layer.id();
     double     layer_rmax = x_layer.rmax();
     double     layer_rmin = x_layer.rmin();
     double     layer_thickness = x_layer.thickness();
+    double     layer_zpos = x_layer.zpos();
 
     Material   l_mat   = description.material(x_layer.materialStr());
 
 
+    DetElement disk_ele("disk_ele", layer_id);
+    Volume disk(x_layer.nameStr(), Tube(layer_rmin, layer_rmax, layer_thickness / 2, 0.0, 2.0 * M_PI), air);
+    disk.setVisAttributes(description.visAttributes(x_layer.visStr()));
+
     Volume halfdisk("halfdisk", Tube(layer_rmin, layer_rmax, layer_thickness / 2, M_PI / 2, M_PI * 3 / 2), l_mat);
     halfdisk.setVisAttributes(description.visAttributes(x_layer.visStr()));
-    PlacedVolume s_phv1 = endcapVol.placeVolume(halfdisk, Position(-disksGap / 2, 0, 0));
+    PlacedVolume s_phv1 = disk.placeVolume(halfdisk, Position(-disksGap / 2, 0, 0));
     s_phv1.addPhysVolID("halfdisk", 0);
-    PlacedVolume s_phv2 = endcapVol.placeVolume(halfdisk, Transform3D(RotationZYX(M_PI, 0, 0), Position(+disksGap / 2, 0, 0)));
+    PlacedVolume s_phv2 = disk.placeVolume(halfdisk, Transform3D(RotationZYX(M_PI, 0, 0), Position(+disksGap / 2, 0, 0)));
     s_phv2.addPhysVolID("halfdisk", 1);
+
+    //disk_ele.setPlacement(s_phv1);
+    //disk_ele.setPlacement(s_phv2);
+
+
+    PlacedVolume s_phv = endcapVol.placeVolume(disk, Position(0, 0, -layer_thickness/2-layer_zpos));
+    s_phv.addPhysVolID(x_layer.nameStr(), layer_id);
+    disk_ele.setPlacement(s_phv);
 
   }
 
