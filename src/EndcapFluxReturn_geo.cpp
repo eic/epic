470aba49da249cf937770e141f3906b492866f4e
Kosarzewski <kosarzewski.1@asc.ohio-state.edu>
2024-01-04 20:18:42 -0500

    Simplified the implementation - no overlaps.
    Signed-off-by: Kosarzewski <kosarzewski.1@asc.ohio-state.edu>

--- src/EndcapFluxReturn_geo.cpp
+++ src/EndcapFluxReturn_geo.cpp
@@ -9,43 +9,22 @@ using namespace dd4hep::detail;
 static Ref_t create_detector(Detector& description, xml_h e, [[maybe_unused]] SensitiveDetector sens)
 {
   xml_det_t x_det    = e;
-  xml_dim_t dim      = x_det.dimensions();
+  //xml_dim_t dim      = x_det.dimensions();
   int       det_id   = x_det.id();
   string    det_name = x_det.nameStr();
   Material  air      = description.air();
-  // int            numsides = dim.numsides();
-  xml::Component pos  = x_det.position();
-  double         rmin = dim.rmin();
-  double         rmax = dim.rmax();
-  double         zmin = dim.zmin();
+  xml::Component x_pos  = x_det.position();
 
 
-  double         rmin_oculus = getAttrOrDefault(dim, _Unicode(rmin_oculus), 0.);
-  double         rmax_oculus = getAttrOrDefault(dim, _Unicode(rmax_oculus), 0.);
-  double         thickness_oculus = getAttrOrDefault(dim, _Unicode(thickness_oculus), 0.);
-  double         rmin_flux = getAttrOrDefault(dim, _Unicode(rmin_flux), 0.);
-  double         rmax_flux = getAttrOrDefault(dim, _Unicode(rmax_flux), 0.);
-  double         thickness_flux = getAttrOrDefault(dim, _Unicode(thickness_flux), 0.);
-
+  Assembly     assembly(det_name);
+  DetElement   sdet(det_name, det_id);
+  PlacedVolume pv;
 
   double disksGap = 0.0;
 
   Layering layering(x_det);
-  double   totalThickness = layering.totalThickness();
+  //double   totalThickness = layering.totalThickness();
 
-  Tube tube_collar(rmin, rmax, totalThickness);
-  Tube tube_oculus(rmin_oculus, rmax_oculus, thickness_oculus);
-  Tube tube_flux(rmin_flux, rmax_flux, thickness_flux);
-
-  UnionSolid endcapSolid(UnionSolid(tube_collar, tube_oculus), tube_flux);
-
-  Volume endcapVol("endcapFlux", endcapSolid, air); // rmax + 2 * cm -to account for disk gap
-  DetElement endcap("endcapFlux", det_id);
-
-  endcapVol.setAttributes(description, x_det.regionStr(), x_det.limitsStr(), x_det.visStr());
-
-  //int    iLayer  = 0;
-  //double globalZ = zmin;
   // Looping through all the different layer sections
   for (xml_coll_t xc(x_det, _U(layer)); xc; ++xc) {
     xml_comp_t x_layer         = xc;
@@ -70,33 +49,18 @@ static Ref_t create_detector(Detector& description, xml_h e, [[maybe_unused]] Se
     PlacedVolume s_phv2 = disk.placeVolume(halfdisk, Transform3D(RotationZYX(M_PI, 0, 0), Position(+disksGap / 2, 0, 0)));
     s_phv2.addPhysVolID("halfdisk", 1);
 
-    //disk_ele.setPlacement(s_phv1);
-    //disk_ele.setPlacement(s_phv2);
 
-
-    PlacedVolume s_phv = endcapVol.placeVolume(disk, Position(0, 0, -layer_thickness/2-layer_zpos));
-    s_phv.addPhysVolID(x_layer.nameStr(), layer_id);
-    disk_ele.setPlacement(s_phv);
-
+    pv = assembly.placeVolume(disk, Position(0, 0, -layer_thickness/2-layer_zpos));
+    pv.addPhysVolID(x_layer.nameStr(), layer_id);
+    disk_ele.setPlacement(pv);
   }
 
-  double       z_pos = zmin + totalThickness / 2;
-  PlacedVolume pv;
-  // Reflect it.
-  Assembly   assembly(det_name);
-  DetElement endcapAssyDE(det_name, det_id);
-  Volume     motherVol = description.pickMotherVolume(endcapAssyDE);
-
-	pv = assembly.placeVolume(endcapVol, Transform3D(RotationZYX(0, 0, 0), Position(0, 0, -z_pos)));
-	pv.addPhysVolID("barrel", 1);
-	Ref_t(endcap)->SetName((det_name + "_backward").c_str());
-	endcap.setPlacement(pv);
-
-  endcapAssyDE.add(endcap);
-  pv = motherVol.placeVolume(assembly, Position(pos.x(), pos.y(), pos.z()));
-  pv.addPhysVolID("system", det_id);
-  endcapAssyDE.setPlacement(pv);
-  return endcapAssyDE;
+  // Get position and place volume
+  Position       pos(x_pos.x(), x_pos.y(), x_pos.z());
+  pv = description.pickMotherVolume(sdet).placeVolume(assembly, pos);
+  pv.addPhysVolID("system", det_id).addPhysVolID("barrel", 0);
+  sdet.setPlacement(pv);
+  return sdet;
 }
 
 // clang-format off
