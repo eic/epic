a817e2f7362f58d45d5710de9196bfbdbc9fdbfe
Kosarzewski <kosarzewski.1@asc.ohio-state.edu>
2024-01-23 16:15:13 -0500

    Removed namespace declarations
    Signed-off-by: Kosarzewski <kosarzewski.1@asc.ohio-state.edu>

--- src/EndcapFluxReturn_geo.cpp
+++ src/EndcapFluxReturn_geo.cpp
@@ -10,27 +10,25 @@
 #include "DD4hep/DetFactoryHelper.h"
 #include "TVector3.h"
 #include "XML/Layering.h"
-using namespace std;
+/*using namespace std;
 using namespace dd4hep;
 using namespace dd4hep::detail;
+*/
 
-
-static Ref_t create_detector(Detector& description, xml_h e, [[maybe_unused]] SensitiveDetector sens)
+static dd4hep::Ref_t create_detector(dd4hep::Detector& description, xml_h e, [[maybe_unused]] dd4hep::SensitiveDetector sens)
 {
   xml_det_t x_det    = e;
   int       det_id   = x_det.id();
-  string    det_name = x_det.nameStr();
-  Material  air      = description.air();
-  xml::Component x_pos  = x_det.position();
+  std::string    det_name = x_det.nameStr();
+  dd4hep::Material  air      = description.air();
+  xml_comp_t x_pos  = x_det.position();
 
 
-  Assembly     assembly(det_name);
-  DetElement   sdet(det_name, det_id);
-  PlacedVolume pv;
+  dd4hep::Assembly     assembly(det_name);
+  dd4hep::DetElement   sdet(det_name, det_id);
+  dd4hep::PlacedVolume pv;
 
   double disksGap = 0.0;
-
-  Layering layering(x_det);
 
   // Looping through all the different layer sections
   for (xml_coll_t xc(x_det, _U(layer)); xc; ++xc) {
@@ -42,28 +40,28 @@ static Ref_t create_detector(Detector& description, xml_h e, [[maybe_unused]] Se
     double     layer_thickness = x_layer.thickness();
     double     layer_zpos = x_layer.zpos();
 
-    Material   l_mat   = description.material(x_layer.materialStr());
+    dd4hep::Material   l_mat   = description.material(x_layer.materialStr());
 
 
-    DetElement disk_ele("disk_ele", layer_id);
-    Volume disk(x_layer.nameStr(), Tube(layer_rmin, layer_rmax, layer_thickness / 2, 0.0, 2.0 * M_PI), air);
+    dd4hep::DetElement disk_ele("disk_ele", layer_id);
+    dd4hep::Volume disk(x_layer.nameStr(), dd4hep::Tube(layer_rmin, layer_rmax, layer_thickness / 2, 0.0, 2.0 * M_PI), air);
     disk.setVisAttributes(description.visAttributes(x_layer.visStr()));
 
-    Volume halfdisk("halfdisk", Tube(layer_rmin, layer_rmax, layer_thickness / 2, M_PI / 2, M_PI * 3 / 2), l_mat);
+    dd4hep::Volume halfdisk("halfdisk", dd4hep::Tube(layer_rmin, layer_rmax, layer_thickness / 2, M_PI / 2, M_PI * 3 / 2), l_mat);
     halfdisk.setVisAttributes(description.visAttributes(x_layer.visStr()));
-    PlacedVolume s_phv1 = disk.placeVolume(halfdisk, Position(-disksGap / 2, 0, 0));
+    dd4hep::PlacedVolume s_phv1 = disk.placeVolume(halfdisk, dd4hep::Position(-disksGap / 2, 0, 0));
     s_phv1.addPhysVolID("halfdisk", 0);
-    PlacedVolume s_phv2 = disk.placeVolume(halfdisk, Transform3D(RotationZYX(M_PI, 0, 0), Position(+disksGap / 2, 0, 0)));
+    dd4hep::PlacedVolume s_phv2 = disk.placeVolume(halfdisk, dd4hep::Transform3D(dd4hep::RotationZYX(M_PI, 0, 0), dd4hep::Position(+disksGap / 2, 0, 0)));
     s_phv2.addPhysVolID("halfdisk", 1);
 
 
-    pv = assembly.placeVolume(disk, Position(0, 0, -layer_thickness/2-layer_zpos));
+    pv = assembly.placeVolume(disk, dd4hep::Position(0, 0, -layer_thickness/2-layer_zpos));
     pv.addPhysVolID(x_layer.nameStr(), layer_id);
     disk_ele.setPlacement(pv);
   }
 
   // Get position and place volume
-  Position       pos(x_pos.x(), x_pos.y(), x_pos.z());
+  dd4hep::Position       pos(x_pos.x(), x_pos.y(), x_pos.z());
   pv = description.pickMotherVolume(sdet).placeVolume(assembly, pos);
   pv.addPhysVolID("system", det_id).addPhysVolID("barrel", 0);
   sdet.setPlacement(pv);
