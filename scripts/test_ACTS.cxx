cc3df744e37fe38fe7aada4edfb4227e8a702408
Wouter Deconinck <wdconinc@gmail.com>
2024-02-17 12:10:03 -0600

    Update scripts/test_ACTS.cxx
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- scripts/test_ACTS.cxx
+++ scripts/test_ACTS.cxx
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2022 Whitney Armstrong
+// Copyright (C) 2022 - 2024, Whitney Armstrong, Wouter Deconinck
 
 #include "DD4hep/Detector.h"
 
