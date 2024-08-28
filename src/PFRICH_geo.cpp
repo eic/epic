a77c41c6212bbaa1c1030577b5fc74b50bbbfa13
Roman Tkachenko <severus@tuta.io>
2024-08-28 11:37:09 +0300

    Move sensor material parameters to XML file

--- src/PFRICH_geo.cpp
+++ src/PFRICH_geo.cpp
@@ -334,10 +334,10 @@ static Ref_t createDetector(Detector& description, xml_h e, SensitiveDetector se
   /*--------------------------------------------------*/
   // HRPPD material definition:
 
-  auto HRPPD_WindowMat = description.material("Quartz");
-  auto HRPPD_PCBMat    = description.material("G10");
-  auto HRPPD_MPDMat    = description.material("SiliconDioxide");
-  auto HRPPD_ASICMat   = description.material("SiliconCarbide");
+  auto HRPPD_WindowMat = description.material(description.constant<std::string>("PFRICH_HRPPD_WindowMat"));
+  auto HRPPD_PCBMat    = description.material(description.constant<std::string>("PFRICH_HRPPD_PCBMat"));
+  auto HRPPD_MPDMat    = description.material(description.constant<std::string>("PFRICH_HRPPD_MPDMat"));
+  auto HRPPD_ASICMat   = description.material(description.constant<std::string>("PFRICH_HRPPD_ASICMat"));
 
   Box hrppd_Solid(xysize / 2, xysize / 2, hrppd_container_volume_thickness / 2);
 
