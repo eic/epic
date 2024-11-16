2f9873e634e1ddcd6dbe698a2372fc28c9d4f6e2
ShujieL <ShujieL@users.noreply.github.com>
2024-11-16 13:59:59 -0800

    Update src/SVTBarrelTracker_geo.cpp
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- src/SVTBarrelTracker_geo.cpp
+++ src/SVTBarrelTracker_geo.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2022 Whitney Armstrong, Jonathan Witte, Shujie Li
+// Copyright (C) 2022-2024 Whitney Armstrong, Jonathan Witte, Shujie Li
 
 /** Curved Silicon Vertex Tracker Barrel with RSU.
  *
