a498b691e46e04e27aa0efbdb9754df0f93e4832
Alexander Kiselev <ayk@bnl.gov>
2025-03-29 23:39:33 -0400

    QRICH work in progress

--- src/QRICH_geo.cpp
+++ src/QRICH_geo.cpp
@@ -8,34 +8,44 @@
 #define _WITH_OPTICS_
 
 // Mimic ePIC pfRICH vessel length;
-#define _FIDUCIAL_VOLUME_LENGTH_SIMPLE_    (500.0*mm)
+#define _FIDUCIAL_VOLUME_LENGTH_               (500.0*mm)
 // Choose ePIC dRICH location;
-#define _FIDUCIAL_VOLUME_OFFSET_SIMPLE_   (1550.0*mm)
-#define _FAKE_HRPPD_PITCH_                  (3.25*mm)
+#define _FIDUCIAL_VOLUME_OFFSET_              (1550.0*mm)
+
+//+#define _FAKE_HRPPD_MATRIX_DIM_                       (1)
+#define _FAKE_HRPPD_MATRIX_DIM_                       (9)
+#define _FAKE_HRPPD_PITCH_                      (3.25*mm)
 // This one does not really matter;
-#define _FIDUCIAL_VOLUME_WIDTH_SIMPLE_    (705*_FAKE_HRPPD_PITCH_)
+//#define _FIDUCIAL_VOLUME_WIDTH_ (705*_FAKE_HRPPD_PITCH_)
+//#define _FAKE_HRPPD_TILE_SIZE_          (_FIDUCIAL_VOLUME_WIDTH_ - 3*_FAKE_HRPPD_PITCH_)
+//#define _FAKE_HRPPD_ACTIVE_AREA_SIZE_   (_FIDUCIAL_VOLUME_WIDTH_ - 5*_FAKE_HRPPD_PITCH_)
+//#define _FAKE_HRPPD_ACTIVE_AREA_SIZE_   (700*_FAKE_HRPPD_PITCH_)
+#define _FAKE_HRPPD_ACTIVE_AREA_SIZE_   (32*_FAKE_HRPPD_PITCH_)
+#define _FAKE_HRPPD_TILE_SIZE_          (120.0*mm)//_FAKE_HRPPD_ACTIVE_AREA_SIZE_ + 2.0)
+#define _FAKE_HRPPD_INSTALLATION_GAP_            (3.0*mm)
+#define _FAKE_HRPPD_INSTALLATION_PITCH_ (_FAKE_HRPPD_TILE_SIZE_ + _FAKE_HRPPD_INSTALLATION_GAP_)
+#define _FIDUCIAL_VOLUME_WIDTH_ (_FAKE_HRPPD_TILE_SIZE_*_FAKE_HRPPD_MATRIX_DIM_ + \
+				 _FAKE_HRPPD_INSTALLATION_GAP_*(_FAKE_HRPPD_MATRIX_DIM_+1) + 50.0*mm)
 
-#define _VESSEL_FRONT_SIDE_THICKNESS_        (5.0*mm)
-#define _SENSOR_AREA_LENGTH_                 (5.0*cm)
+#define _VESSEL_FRONT_SIDE_THICKNESS_            (5.0*mm)
+#define _SENSOR_AREA_LENGTH_                     (5.0*cm)
 
-#define _BUILDING_BLOCK_CLEARANCE_           (1.0*mm)
+#define _BUILDING_BLOCK_CLEARANCE_               (1.0*mm)
 
-#define _AEROGEL_THICKNESS_                  (2.5*cm)
-#define _ACRYLIC_THICKNESS_                  (3.0*mm)
+#define _AEROGEL_THICKNESS_                      (2.5*cm)
+#define _ACRYLIC_THICKNESS_                      (3.0*mm)
+
+#define _FAKE_HRPPD_WINDOW_THICKNESS_            (5.0*mm)
+#define _FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_    (32.0*mm)
+
+#define _FAKE_SENSOR_PLANE_GEOMETRIC_EFFICIENCY_   (1.00)
+#define _FAKE_SAFETY_FACTOR_                       (0.70)
 
 using namespace dd4hep;
 
 #ifdef _WITH_OPTICS_
 #include "IRT/CherenkovDetectorCollection.h"
 #endif
-
-#define _FAKE_HRPPD_WINDOW_THICKNESS_            (5.0*mm)
-#define _FAKE_HRPPD_TILE_SIZE_          (_FIDUCIAL_VOLUME_WIDTH_SIMPLE_ - 3*_FAKE_HRPPD_PITCH_)
-#define _FAKE_HRPPD_ACTIVE_AREA_SIZE_   (_FIDUCIAL_VOLUME_WIDTH_SIMPLE_ - 5*_FAKE_HRPPD_PITCH_)
-#define _FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_    (32.0*mm)
-
-#define _FAKE_SENSOR_PLANE_GEOMETRIC_EFFICIENCY_   (1.00)
-#define _FAKE_SAFETY_FACTOR_                       (0.70)
 
 static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetector sens)
 {
@@ -101,8 +111,8 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
     return enc;
   };
 
-  const double fvLength = _FIDUCIAL_VOLUME_LENGTH_SIMPLE_, fvWidth  = _FIDUCIAL_VOLUME_WIDTH_SIMPLE_;
-  const double fvOffset = _FIDUCIAL_VOLUME_OFFSET_SIMPLE_;
+  const double fvLength = _FIDUCIAL_VOLUME_LENGTH_, fvWidth  = _FIDUCIAL_VOLUME_WIDTH_;
+  const double fvOffset = _FIDUCIAL_VOLUME_OFFSET_;
   
   // Fiducial volume / vessel;
   auto vesselSolid = Box(fvWidth/2, fvWidth/2, fvLength/2);
@@ -133,8 +143,6 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
       auto boundary = new FlatSurface(TVector3(0,0,0), /*sign**/TVector3(1,0,0), TVector3(0,-1,0));
       
       auto radiator = geometry->SetContainerVolume(cdet, "GasVolume", 0, (G4LogicalVolume*)(0x0), 0, boundary);
-      // FIXME: hardcoded;
-      radiator->SetReferenceRefractiveIndex(1.00033);
       radiator->SetAlternativeMaterialName(gasvolMaterialName.c_str());
     }
 #endif
@@ -161,7 +169,7 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
 	  auto radiator = geometry->AddFlatRadiator(cdet, "Aerogel", CherenkovDetector::Upstream, 
 						      0, (G4LogicalVolume*)(0x1), 0, surface, agThick/mm);
 	  radiator->SetAlternativeMaterialName(aerogelMaterialName.c_str());
-	  // FIXME: what is it good for?;
+	  // FIXME: what is it good for in ePIC IRT 2.0 implementation?;
 	  geometry->AddRadiatorLogicalVolume(radiator, (G4LogicalVolume*)(0x1));//ag_log);
 	}
 #endif
@@ -187,8 +195,6 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
 	  
 	  auto radiator = geometry->AddFlatRadiator(cdet, "Acrylic", CherenkovDetector::Upstream, 
 						    0, (G4LogicalVolume*)(0x2), 0, surface, acThick/mm);
-	  // FIXME: hardcoded;
-	  radiator->SetReferenceRefractiveIndex(1.5017);
 	  radiator->SetAlternativeMaterialName(acrylicMaterialName.c_str());
 	}
 #endif
@@ -198,14 +204,6 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
 
   // A fake HRPPD container volume, window, photocathode;
   {
-    // Mimic BuildFakePhotonDetectorMatrix() used in the standalone code;
-    const double fvzOffset = fvOffset, wzOffset = fvLength/2 - _SENSOR_AREA_LENGTH_;
-    const double pdthick = 0.01*mm, zpdc = wzOffset + _FAKE_HRPPD_WINDOW_THICKNESS_ + pdthick/2;
-    auto pd_box  = Box(/*"PhotoDetector",*/ _FAKE_HRPPD_ACTIVE_AREA_SIZE_/2,
-		       _FAKE_HRPPD_ACTIVE_AREA_SIZE_/2, pdthick/2);
-    Volume pcVol(detName + "_photodetector", pd_box, pcMaterial);
-    pcVol.setSensitiveDetector(sens);
-    
 #ifdef _WITH_OPTICS_
     //auto pd = new CherenkovPhotonDetector(pd_box, m_Bialkali);
     // [0,0]: have neither access to G4VSolid nor to G4Material; IRT code does not care; fine;
@@ -213,14 +211,23 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
     
     // FIXME: '0' stands for the unknown (and irrelevant) G4LogicalVolume;
     geometry->AddPhotonDetector(cdet, 0, pd);
+    
+    // FIXME: really needed in EICrecon?;
+    pd->SetCopyIdentifierLevel(1);
+    //?pd->DefineLogicalVolume();
+    // Cannot access GEANT shapes in the reconstruction code -> store this value;
+    pd->SetActiveAreaSize(_FAKE_HRPPD_ACTIVE_AREA_SIZE_/mm);
+    
+    pd->SetGeometricEfficiency(_FAKE_SENSOR_PLANE_GEOMETRIC_EFFICIENCY_ * _FAKE_SAFETY_FACTOR_);
 #endif
     
-    // Full size quartz window; FIXME: Sapphire, here and in all other places;
-    auto wnd_box = Box(/*"QuartzWindow",*/ _FAKE_HRPPD_TILE_SIZE_/2, _FAKE_HRPPD_TILE_SIZE_/2,
-		       _FAKE_HRPPD_WINDOW_THICKNESS_/2);
-    Volume wnd_log(detName + "_wnd", wnd_box, windowMaterial);
+    // Mimic BuildFakePhotonDetectorMatrix() used in the standalone code;
+    const double fvzOffset = fvOffset, wzOffset = fvLength/2 - _SENSOR_AREA_LENGTH_;
+    const double pdthick = 0.01*mm, zpdc = wzOffset + _FAKE_HRPPD_WINDOW_THICKNESS_ + pdthick/2;
     
-    //+auto hrppd_log = BuildFakeHRPPD(wnd_log, pd_box, pd);
+    //+auto hrppd_log = BuildFakeHRPPD(wndVol, pd_box, pd);
+
+    // For now assume it is a unique surface, same for all HRPPDs;
 #ifdef _WITH_OPTICS_
     {	
       TVector3 nx(1,0,0), ny(0,-1,0);
@@ -229,112 +236,119 @@ static Ref_t createDetector(Detector& desc, xml::Handle_t handle, SensitiveDetec
 	new FlatSurface((1/mm)*TVector3(0,0,fvzOffset + wzOffset + _FAKE_HRPPD_WINDOW_THICKNESS_/2), nx, ny);
       
       auto radiator = geometry->AddFlatRadiator(cdet, "QuartzWindow", CherenkovDetector::Downstream, 
-						0, (G4LogicalVolume*)(0x3), 0, /*wnd_log, m_FusedSilica,*/ surface,
+						0, (G4LogicalVolume*)(0x3), 0, /*wndVol, m_FusedSilica,*/ surface,
 						_FAKE_HRPPD_WINDOW_THICKNESS_/mm);
-      radiator->SetReferenceRefractiveIndex(1.46);//1.00029);
       radiator->SetAlternativeMaterialName(windowMaterialName.c_str());
     }	
 #endif
-    
+        
     //?m_Geometry->AddPhotonDetector(cdet, pd->GetLogicalVolume(), pd);
-    {
-      auto hrppd_box =
-	Box(_FAKE_HRPPD_TILE_SIZE_/2, _FAKE_HRPPD_TILE_SIZE_/2, _FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_/2);
-      // FIXME: think (OLD: well, let it be vacuum inside);
-      Volume hrppd_log(detName + "_hrppd", hrppd_box, gasvolMaterial);//vacuumMat);
+    for(unsigned ix=0; ix<_FAKE_HRPPD_MATRIX_DIM_; ix++) {
+      double xOffset = _FAKE_HRPPD_INSTALLATION_PITCH_*(ix - (_FAKE_HRPPD_MATRIX_DIM_-1)/2.);
       
-#ifdef _WITH_OPTICS_
-      // FIXME: really needed in EICrecon?;
-      pd->SetCopyIdentifierLevel(1);
-      //?pd->DefineLogicalVolume();
-      // Cannot access GEANT shapes in the reconstruction code -> store this value;
-      pd->SetActiveAreaSize(_FAKE_HRPPD_ACTIVE_AREA_SIZE_/mm);
-      
-      pd->SetGeometricEfficiency(_FAKE_SENSOR_PLANE_GEOMETRIC_EFFICIENCY_ * _FAKE_SAFETY_FACTOR_);
-#endif
-      
-#if _LATER_
-      auto qd_box  =
-	Box(_FAKE_HRPPD_ACTIVE_AREA_SIZE_/2, _FAKE_HRPPD_ACTIVE_AREA_SIZE_/2, pdthick/2);
-      Volume qd_log(detName + "_fake_photodetector", qd_box, bialkaliMat);
-#endif
-      
-      {
-	double accu = -_FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_/2;
+      for(unsigned iy=0; iy<_FAKE_HRPPD_MATRIX_DIM_; iy++) {
+	unsigned id = iy*_FAKE_HRPPD_MATRIX_DIM_ + ix;
+	double yOffset = _FAKE_HRPPD_INSTALLATION_PITCH_*(iy - (_FAKE_HRPPD_MATRIX_DIM_-1)/2.);
+
+	uint64_t sensorID = 0x0;
 	
-	// Window layer;
-	hrppd_log.placeVolume(wnd_log, Position(0, 0, accu + _FAKE_HRPPD_WINDOW_THICKNESS_/2));
-	accu += _FAKE_HRPPD_WINDOW_THICKNESS_;
+	auto hrppdBox =
+	  Box(_FAKE_HRPPD_TILE_SIZE_/2, _FAKE_HRPPD_TILE_SIZE_/2, _FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_/2);
+	// FIXME: think (OLD: well, let it be vacuum inside);
+	TString hrppdName; hrppdName.Form("%s-hrppd-%02d%02d", detName.c_str(), ix, iy);
+	Volume hrppdVol(/*detName + "_hrppd"*/hrppdName.Data(), hrppdBox, gasvolMaterial);//vacuumMat);
 	
-	// Fake photodector layer (photocathode);
-	//new G4PVPlacement(0, G4ThreeVector(0.0, 0.0, accu + pdthick/2), qd_log, "FakePhotoDetector", 
-	//		    hrppd_log, false, 0);
-	//+hrppd_log.placeVolume(qd_log, Position(0, 0, accu + pdthick/2));
-	//+accu += pdthick;
+	// Full size quartz window; 
+	auto wndBox = Box(/*"QuartzWindow",*/ _FAKE_HRPPD_TILE_SIZE_/2, _FAKE_HRPPD_TILE_SIZE_/2,
+			  _FAKE_HRPPD_WINDOW_THICKNESS_/2);
+	TString wndName; wndName.Form("%s-window-%02d%02d", detName.c_str(), ix, iy);
+	Volume wndVol(/*detName + "_wnd"*/wndName.Data(), wndBox, windowMaterial);
 	
-	// Photodector layer (photocathode);
-	auto pcPlacement = Position(0, 0, accu + pdthick/2);
-	auto pcPV = hrppd_log.placeVolume(pcVol, pcPlacement);
-	accu += pdthick;
+	auto pcBox  = Box(/*"PhotoDetector",*/ _FAKE_HRPPD_ACTIVE_AREA_SIZE_/2,
+			  _FAKE_HRPPD_ACTIVE_AREA_SIZE_/2, pdthick/2);
+	TString pcName; pcName.Form("%s-photocathode-%02d%02d", detName.c_str(), ix, iy);
+	Volume pcVol(/*detName + "_photodetector"*/pcName.Data(), pcBox, pcMaterial);
+	pcVol.setSensitiveDetector(sens);
+	
 	{
-	  double pcThickness = pdthick;
-	  double resinSide = _FAKE_HRPPD_ACTIVE_AREA_SIZE_ + 1*mm, resinThickness = pcThickness + 1*mm;
-	  Box resinSolid(resinSide / 2., resinSide / 2., resinThickness / 2.);
+	  double accu = -_FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_/2;
 	  
-	  auto resinPlacement = Position(0, 0, accu + resinThickness/2);
-	  Volume resinVol("_resin_", resinSolid/*Embedded*/, resinMat);
-	  hrppd_log.placeVolume(resinVol, resinPlacement);
-	}
-	{
-	  // sensor readout // NOTE: follow `sensorIDfields`
-	  pcPV.addPhysVolID("pdu", 0);
+	  // Window layer;
+	  hrppdVol.placeVolume(wndVol, Position(0.0, 0.0, accu + _FAKE_HRPPD_WINDOW_THICKNESS_/2));
+	  accu += _FAKE_HRPPD_WINDOW_THICKNESS_;
 	  
-	  // sensor DetElement
-	  auto sensorID = encodeSensorID(pcPV.volIDs());
-	  printf("@S@: %lu\n", sensorID); 
-	  //std::string sensorIDname =
-	  //secName + "_pdu" + std::to_string(ipdu) + "_sipm" + std::to_string(isipm);
-	  DetElement pcDE(det, "sensor_de_"/* + sensorIDname*/, sensorID);
-	  pcDE.setPlacement(pcPV);
-	}
-      }
-            
-      vesselVolume.placeVolume(hrppd_log, Position(0, 0, wzOffset + _FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_/2));
-    }
-    
-#ifdef _WITH_OPTICS_
-    {
-      // Photocathode surface;
-      auto surface = new FlatSurface((1/mm)*TVector3(0.0, 0.0, fvzOffset + zpdc),
-				     TVector3(1,0,0), TVector3(0,-1,0));
-      
-      {
-	// Mimic det->CreatePhotonDetectorInstance();
-	unsigned sector = 0, icopy = 0;
-	auto irt = pd->AllocateIRT(sector, icopy);
-	
-	// Aerogel and acrylic;
-	if (cdet->m_OpticalBoundaries[CherenkovDetector::Upstream].find(sector) != 
-	    cdet->m_OpticalBoundaries[CherenkovDetector::Upstream].end())
-	  for(auto boundary: cdet->m_OpticalBoundaries[CherenkovDetector::Upstream][sector])
-	    irt->AddOpticalBoundary(boundary);
-	
-	// Fused silica windows;
-	if (cdet->m_OpticalBoundaries[CherenkovDetector::Downstream].find(sector) != 
-	    cdet->m_OpticalBoundaries[CherenkovDetector::Downstream].end())
-	  for(auto boundary: cdet->m_OpticalBoundaries[CherenkovDetector::Downstream][sector])
-	    irt->AddOpticalBoundary(boundary);
-	
-	// Terminate the optical path;
-	pd->AddItselfToOpticalBoundaries(irt, surface);
-      }
-    } 
+	  // Photocathode layer;
+	  //auto pcPlacement = Position(0, 0, accu + pdthick/2);
+	  {
+	    auto pcPV = hrppdVol.placeVolume(pcVol, Position(0.0, 0.0, accu + pdthick/2));//pcPlacement);
+	    // sensor readout // NOTE: follow `sensorIDfields`
+	    pcPV.addPhysVolID("pdu", id);//+1);//0);
+	    
+	    // sensor DetElement
+	    /*auto*/ sensorID = encodeSensorID(pcPV.volIDs());
+	    //printf("@S@: %lu\n", sensorID); 
+	    //std::string sensorIDname =
+	    //secName + "_pdu" + std::to_string(ipdu) + "_sipm" + std::to_string(isipm);
+	    TString deName; deName.Form("%s-sensor-%02d%02d", detName.c_str(), ix, iy);
+	    DetElement pcDE(det, deName.Data()/*"sensor_de_"*//* + sensorIDname*/, sensorID);
+	    pcDE.setPlacement(pcPV);
+	  }
+	  accu += pdthick;
+#if 1
+	  // A fake absorber layer behind the photocathode; FIXME: make sure that reflection
+	  // on the window and photocathode boundary still works correctly (no fake volume as
+	  // in a standalone code);
+	  TString absName; absName.Form("%s-absorber-%02d%02d", detName.c_str(), ix, iy);
+	  // Recycle the same pcBox shape;
+	  Volume absVol(absName.Data(), pcBox, resinMat);//pcMaterial);
+	  hrppdVol.placeVolume(absVol, Position(0.0, 0.0, accu + pdthick/2));//resinPlacement);
+#else
+	  {
+	    double pcThickness = pdthick;
+	    double resinSide = _FAKE_HRPPD_ACTIVE_AREA_SIZE_ + 1*mm, resinThickness = pcThickness + 1*mm;
+	    Box resinSolid(resinSide / 2., resinSide / 2., resinThickness / 2.);
+	    
+	    auto resinPlacement = Position(0, 0, accu + resinThickness/2);
+	    Volume resinVol("_resin_", resinSolid/*Embedded*/, resinMat);
+	    hrppdVol.placeVolume(resinVol, resinPlacement);
+	  }
 #endif
-    
-    //for(auto radiator: cdet->Radiators())
-    //radiator.second->SetReferenceRefractiveIndex(radiator.second->GetMaterial()->RefractiveIndex(eV*_MAGIC_CFF_/_LAMBDA_NOMINAL_));
+	}
+	
+	vesselVolume.placeVolume(hrppdVol, Position(xOffset, yOffset, wzOffset + _FAKE_HRPPD_CONTAINER_VOLUME_HEIGHT_/2));
+	
+#ifdef _WITH_OPTICS_
+	{
+	  // Photocathode surface;
+	  auto surface = new FlatSurface((1/mm)*TVector3(xOffset, yOffset, fvzOffset + zpdc),
+					 TVector3(1,0,0), TVector3(0,-1,0));
+	  
+	  {
+	    // Mimic det->CreatePhotonDetectorInstance();
+	    unsigned sector = 0, icopy = sensorID;//0;
+	    auto irt = pd->AllocateIRT(sector, icopy);
+	    
+	    // Aerogel and acrylic;
+	    if (cdet->m_OpticalBoundaries[CherenkovDetector::Upstream].find(sector) != 
+		cdet->m_OpticalBoundaries[CherenkovDetector::Upstream].end())
+	      for(auto boundary: cdet->m_OpticalBoundaries[CherenkovDetector::Upstream][sector])
+		irt->AddOpticalBoundary(boundary);
+	    
+	    // Fused silica windows;
+	    if (cdet->m_OpticalBoundaries[CherenkovDetector::Downstream].find(sector) != 
+		cdet->m_OpticalBoundaries[CherenkovDetector::Downstream].end())
+	      for(auto boundary: cdet->m_OpticalBoundaries[CherenkovDetector::Downstream][sector])
+		irt->AddOpticalBoundary(boundary);
+	    
+	    // Terminate the optical path;
+	    pd->AddItselfToOpticalBoundaries(irt, surface);
+	  }
+	} 
+#endif
+      } //for iy
+    } //for ix
   }
-    
+      
   // Write out optical configuration;
 #ifdef _WITH_OPTICS_
   geometry->Write();
