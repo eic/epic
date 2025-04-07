256c0208d1795b48bac9b63ef81a9ba029cff31c
Andrii Natochii <natochii@bnl.gov>
2025-04-06 23:10:54 -0400

    Added a racetrack section to the upstream lepton beam pipe

--- src/IP6BeamPipe.cpp
+++ src/IP6BeamPipe.cpp
@@ -69,10 +69,11 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 	VisAttr IPwallVis("IPwall");
 	VisAttr IPcoatingVis("IPcoating");
 
-    	wallVis.setColor(0.0, 0.0, 1.0, 1.0);  // r, g, b, alpha
-    	coatingVis.setColor(1.0, 0.0, 0.0, 1.0);
-    	IPwallVis.setColor(0.0, 1.0, 0.0, 1.0);
-    	IPcoatingVis.setColor(1.0, 1.0, 0.0, 1.0);
+	// colors: (r, g, b, alpha)
+    	wallVis.setColor(0.0, 0.0, 1.0, 1.0);  // blue
+    	coatingVis.setColor(1.0, 0.0, 0.0, 1.0); // red
+    	IPwallVis.setColor(0.0, 1.0, 0.0, 1.0); // green
+    	IPcoatingVis.setColor(1.0, 1.0, 0.0, 1.0); // yellow
 
 	// central acts beampipe volume
 	Tube central_tube(
@@ -138,11 +139,13 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 	Volume v_upstream_IP_coating("v_upstream_IP_coating", upstream_IP_coating, IP_beampipe_coating_material);
 	Volume v_upstream_IP_tube("v_upstream_IP_tube", upstream_IP_tube, IP_beampipe_wall_material);
 
+	// set vis colors
 	v_downstream_IP_tube.setVisAttributes(IPwallVis);
 	v_upstream_IP_tube.setVisAttributes(IPwallVis);
 	v_downstream_IP_coating.setVisAttributes(IPcoatingVis);
 	v_upstream_IP_coating.setVisAttributes(IPcoatingVis);
 
+	// set names
 	sdet.setAttributes(det, v_upstream_IP_coating, x_det.regionStr(), x_det.limitsStr(), vis_name);
 	sdet.setAttributes(det, v_upstream_IP_tube, x_det.regionStr(), x_det.limitsStr(), vis_name);
 	sdet.setAttributes(det, v_downstream_IP_coating, x_det.regionStr(), x_det.limitsStr(), vis_name);
@@ -175,9 +178,9 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 		{
 			xml_comp_t x_zplane = x_zplane_i;
 			// thickness
-			auto wall_thickness 	= 
+			auto wall_thickness = 
 				getAttrOrDefault(x_zplane, _Unicode(wall_thickness), 1 * mm);
-			auto coating_thickness 	= 
+			auto coating_thickness = 
 				getAttrOrDefault(x_zplane, _Unicode(coating_thickness), 30 * um);
 			// z position
 			z.push_back(x_zplane.attr<double>(_Unicode(z)));
@@ -195,6 +198,56 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 			{0, 2.0 * M_PI, rmin_coating, rmax_coating, z},
 			{0, 2.0 * M_PI, rmin_vacuum, rmax_vacuum, z}
 		);
+	};
+	//---------------------------------------------------------------------------------------------------------
+	// Helper function to create a racetrack volumes 
+  	auto create_racetrack_solids = [&](
+		xml::Component& x_racetrack)
+	{
+		double semiCircle_rmin = getAttrOrDefault(x_racetrack, _Unicode(semiCircle_rmin), 0.0);
+		double wall_thickness = getAttrOrDefault(x_racetrack, _Unicode(wall_thickness), 0.0);
+		double length = getAttrOrDefault(x_racetrack, _Unicode(length), 0.0);
+		double rectangle_h = getAttrOrDefault(x_racetrack, _Unicode(rectangle_h), 0.0);
+		
+		Tube wall_semiCircle(0, semiCircle_rmin +  wall_thickness, length/2.);
+		Box wall_rectangle(semiCircle_rmin + wall_thickness, rectangle_h/2., length/2.);
+
+		Tube coating_semiCircle(0, semiCircle_rmin, length/2.);
+		Box coating_rectangle(semiCircle_rmin, rectangle_h/2., length/2.);
+
+		Tube vacuum_semiCircle(0, semiCircle_rmin -  wall_thickness, length/2.);
+		Box vacuum_rectangle(semiCircle_rmin - wall_thickness, rectangle_h/2., length/2.);
+
+		Transform3D upShift_tf(RotationZYX(0, 0, 0), Position(0, rectangle_h/2., 0));
+		Transform3D downShift_tf(RotationZYX(0, 0, 0), Position(0, -rectangle_h/2., 0));
+
+		UnionSolid wall_solid, coating_solid, vacuum_solid;
+
+		wall_solid = 
+			UnionSolid("wall_solid_tmp",wall_rectangle, wall_semiCircle, upShift_tf);
+		wall_solid = 
+			UnionSolid("wall_solid",wall_solid, wall_semiCircle, downShift_tf);
+
+		coating_solid = 
+			UnionSolid("coating_solid_tmp",coating_rectangle, coating_semiCircle, upShift_tf);
+		coating_solid = 
+			UnionSolid("coating_solid",coating_solid, coating_semiCircle, downShift_tf);
+
+		vacuum_solid = 
+			UnionSolid("vacuum_solid_tmp",vacuum_rectangle, vacuum_semiCircle, upShift_tf);
+		vacuum_solid = 
+			UnionSolid("vacuum_solid",vacuum_solid, vacuum_semiCircle, downShift_tf);
+
+		Transform3D zero_tf(RotationZYX(0, 0, 0), Position(0, 0, 0));
+		BooleanSolid wall_subtract, coating_subtract, vacuum_subtract;
+
+		wall_subtract = 
+			SubtractionSolid("wall_subtract",wall_solid,vacuum_solid,zero_tf);
+		coating_subtract = 
+			SubtractionSolid("coating_subtract",coating_solid,vacuum_solid,zero_tf);
+		vacuum_subtract = vacuum_solid;
+	
+		return std::tuple<Solid,Solid,Solid>(wall_subtract,coating_subtract,vacuum_subtract);
 	};
 	//---------------------------------------------------------------------------------------------------------
 	// Helper function to create union of lepton and hadron volumes
@@ -215,32 +268,25 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 				Transform3D(Position(0, 0, -axis_intersection));
 
 		// union of wall, coating, and vacuum
-		UnionSolid wall_union(		std::get<0>(pipe1_polycones), std::get<0>(pipe2_polycones), tf);
-		UnionSolid coating_union(	std::get<1>(pipe1_polycones), std::get<1>(pipe2_polycones), tf);
-		UnionSolid vacuum_union(	std::get<2>(pipe1_polycones), std::get<2>(pipe2_polycones), tf);
+		BooleanSolid wall_union = UnionSolid(
+			std::get<0>(pipe1_polycones), std::get<0>(pipe2_polycones), tf);
+		BooleanSolid coating_union = UnionSolid(
+			std::get<1>(pipe1_polycones), std::get<1>(pipe2_polycones), tf);
+		BooleanSolid vacuum_union = UnionSolid(
+			std::get<2>(pipe1_polycones), std::get<2>(pipe2_polycones), tf);
 
-		BooleanSolid wall, coating, vacuum;
-
-		if(name == "upstream") // upstream
+		if(name == "downstream")
 		{
-			// subtract vacuum from coating
-			coating = SubtractionSolid(coating_union, vacuum_union);
-			// subtract vacuum from wall
-			wall = SubtractionSolid(wall_union, vacuum_union);
-			// get vacuum
-			vacuum = vacuum_union;
-		}
+			xml::Component racetrack_lepton_c = x_pipe1.child(_Unicode(racetrack_lepton));
+			auto racetrack_solids = create_racetrack_solids(racetrack_lepton_c);
 
-		else // downstream
-		{
-			// subtract wall from vacuum
-			vacuum = SubtractionSolid(vacuum_union, wall_union);
-			// subtract coating from vacuum
-			vacuum = SubtractionSolid(vacuum, coating_union);
-			// get wall
-			wall = wall_union;
-			// get coating
-			coating = coating_union;
+			double offset_z = getAttrOrDefault(racetrack_lepton_c, _Unicode(offset_z), 0.0); 
+			double length = getAttrOrDefault(racetrack_lepton_c, _Unicode(length), 0.0);
+			Transform3D racetrack_tf(RotationZYX(0, 0, 0), Position(0, 0, offset_z + length/2.));
+
+			wall_union = UnionSolid("wall_union",wall_union,std::get<0>(racetrack_solids),racetrack_tf);
+			coating_union = UnionSolid("coating_union",coating_union,std::get<1>(racetrack_solids),racetrack_tf);
+			vacuum_union = UnionSolid("vacuum_union",vacuum_union,std::get<2>(racetrack_solids),racetrack_tf);
 		}
 
 		// subtract additional vacuum from wall and coating
@@ -255,8 +301,42 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 			auto additional_tf = 	Transform3D(Position(0, 0, additional_axis_intersection)) *
 						Transform3D(RotationY(additional_crossing_angle)) *
 						Transform3D(Position(0, 0, -additional_axis_intersection));
-			
-			wall = SubtractionSolid(wall, std::get<1>(additional_polycones), additional_tf);
+		
+			wall_union = SubtractionSolid(
+				wall_union, std::get<2>(additional_polycones), additional_tf);
+			coating_union = SubtractionSolid(
+				coating_union, std::get<2>(additional_polycones), additional_tf);
+			vacuum_union = SubtractionSolid(
+				vacuum_union, std::get<2>(additional_polycones), additional_tf);
+			// unite vacuums
+			if(getAttrOrDefault<bool>(x_additional_subtraction, _Unicode(unite_vacuum), false))
+			{
+				vacuum_union = UnionSolid(
+					vacuum_union, std::get<2>(additional_polycones), additional_tf);
+			}
+		}
+
+		BooleanSolid wall, coating, vacuum;
+
+		if(name == "upstream") // upstream
+		{
+			// subtract vacuum from coating
+			coating = SubtractionSolid(coating_union, vacuum_union);
+			// subtract vacuum from wall
+			wall = SubtractionSolid(wall_union, vacuum_union);
+			// get vacuum
+			vacuum = vacuum_union;
+		}
+		else // downstream
+		{
+			// subtract wall from vacuum
+			vacuum = SubtractionSolid(vacuum_union, wall_union);
+			// subtract coating from vacuum
+			vacuum = SubtractionSolid(vacuum, coating_union);
+			// get wall
+			wall = wall_union;
+			// get coating
+			coating = coating_union;
 		}
 
 		return std::tuple<Volume, Volume, Volume>(
@@ -269,8 +349,8 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 
 	// -----------------------------
 	// Upstream/BWD/Rear Side:
-	// - incoming hadron tube
-	// - outgoing electron tube
+	// - incoming hadron tube: straight
+	// - outgoing electron tube: tapering
 
 	xml::Component upstream_c		= x_det.child(_Unicode(upstream));
 	xml::Component incoming_hadron_c 	= upstream_c.child(_Unicode(incoming_hadron));
@@ -300,35 +380,40 @@ static Ref_t create_detector(Detector& det, xml_h e, SensitiveDetector /* sens *
 		assembly.placeVolume(std::get<2>(volumes_upstream), tf_upstream);
 	}
 
-/*
+	// -----------------------------
+	// Downstream/FWD Side:
+	// - incoming electron tube: tube with tube cut out
+	// - outgoing hadron tube: cone centered at scattering angle
 
-  // -----------------------------
-  // downstream:
-  // - incoming electron tube: tube with tube cut out
-  // - outgoing hadron tube: cone centered at scattering angle
-  // (incoming electron tube internal touching to outgoing hadron tube)
+	xml::Component downstream_c      = x_det.child(_Unicode(downstream));
+	xml::Component incoming_lepton_c = downstream_c.child(_Unicode(incoming_lepton));
+	xml::Component outgoing_hadron_c = downstream_c.child(_Unicode(outgoing_hadron));
+	xml_coll_t additional_subtractions_downstream(downstream_c, _Unicode(additional_subtraction));
+	auto volumes_downstream = create_volumes(
+		"downstream", 
+		incoming_lepton_c, 
+		outgoing_hadron_c,
+		additional_subtractions_downstream);
 
-  xml::Component downstream_c      = x_det.child(_Unicode(downstream));
-  xml::Component incoming_lepton_c = downstream_c.child(_Unicode(incoming_lepton));
-  xml::Component outgoing_hadron_c = downstream_c.child(_Unicode(outgoing_hadron));
-  xml_coll_t additional_subtractions_downstream(downstream_c, _Unicode(additional_subtraction));
-  bool subtract_vacuum_downstream =
-      getAttrOrDefault<bool>(downstream_c, _Unicode(subtract_vacuum), true);
-  bool subtract_matter_downstream =
-      getAttrOrDefault<bool>(downstream_c, _Unicode(subtract_matter), true);
-  auto volumes_downstream = create_volumes("downstream", incoming_lepton_c, outgoing_hadron_c,
-                                           additional_subtractions_downstream,
-                                           subtract_vacuum_downstream, subtract_matter_downstream);
+	std::get<0>(volumes_downstream).setVisAttributes(wallVis);
+	std::get<1>(volumes_downstream).setVisAttributes(coatingVis);
 
-  auto tf_downstream = Transform3D(RotationZYX(0, 0, 0));
-  if (getAttrOrDefault<bool>(downstream_c, _Unicode(reflect), true)) {
-    tf_downstream = Transform3D(RotationZYX(0, M_PI, 0));
-  }
-  assembly.placeVolume(volumes_downstream.first, tf_downstream);
-  if (getAttrOrDefault<bool>(downstream_c, _Unicode(place_vacuum), true)) {
-    assembly.placeVolume(volumes_downstream.second, tf_downstream);
-  }
-*/
+	auto tf_downstream = Transform3D(RotationZYX(0, 0, 0));
+	if (getAttrOrDefault<bool>(downstream_c, _Unicode(reflect), true)) 
+	{
+		tf_downstream = Transform3D(RotationZYX(0, M_PI, 0));
+	}
+
+	// place wall
+	assembly.placeVolume(std::get<0>(volumes_downstream), tf_downstream);
+	// place coating
+	assembly.placeVolume(std::get<1>(volumes_downstream), tf_downstream);
+	// place vacuum
+	if (getAttrOrDefault<bool>(downstream_c, _Unicode(place_vacuum), true)) 
+	{
+		assembly.placeVolume(std::get<2>(volumes_downstream), tf_downstream);
+	}
+
 	// -----------------------------
 	// final placement
 	auto pv_assembly = det.pickMotherVolume(sdet).placeVolume(assembly);
