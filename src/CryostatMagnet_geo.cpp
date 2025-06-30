26727a74380ca5ba37949dc9c0e56339edd0af57
Andrii Natochii <natochii@bnl.gov>
2025-06-30 10:23:13 -0400

    Updated the copyright statements to include 'Andrii Natochii'

--- src/CryostatMagnet_geo.cpp
+++ src/CryostatMagnet_geo.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (C) 2022 Alex Jentsch, Wouter Deconinck, Whitney Armstrong
+// Copyright (C) 2022 Alex Jentsch, Wouter Deconinck, Whitney Armstrong, Andrii Natochii
 
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/Printout.h"
