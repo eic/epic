d4e75af5583ca2c2ccecd44e96855a84b8cddb20
simonge <simon.gardner@glasgow.ac.uk>
2025-07-03 15:31:13 +0100

    Fixes for stuck particles

--- src/BeamPipeChain_geo.cpp
+++ src/BeamPipeChain_geo.cpp
@@ -84,6 +84,7 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector /
     double l      = sqrt(deltaX * deltaX + deltaZ * deltaZ);
     double theta  = atan2(deltaX, deltaZ);
 
+
     xCenters[pipeN] = x;
     zCenters[pipeN] = z;
     lengths[pipeN]  = l;
@@ -94,14 +95,21 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector /
 
   // If there is an bend in the pipe, calculate the length reduction of the pipe and joint length
   for (uint i = 1; i < thetas.size(); i++) {
+
+
+    std::cout << "Pipe " << i<< ": " << names[i] << std::endl;
+    std::cout << "theta: " << thetas[i-1] << std::endl;
+    std::cout << "theta: " << thetas[i] << std::endl;
     // Start at the join between the first two pipes ending at the join between the last two pipes N-1
-    if (thetas[i - 1] == thetas[i]) {
+    double bendAngle  = thetas[i] - thetas[i - 1];
+    if (std::abs(bendAngle) < 0.00001) {
       bendLengths.push_back(0);
     } else // Correct for tubes, not yet cones so imperfect
     {
-      double bendAngle  = thetas[i] - thetas[i - 1];
       double bendLength = abs(rOuters1[i] * tan(bendAngle / 2));
       bendLengths.push_back(bendLength + bendRadius);
+      std::cout << "bendAngle: " << bendAngle << std::endl;
+      std::cout << "bendLength: " << bendLength << std::endl;
     }
   }
 
