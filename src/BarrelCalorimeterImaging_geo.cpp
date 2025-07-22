18f1ccd261cf41a8e1faf2d2a43d99105b8824b3
Wouter Deconinck <wdconinc@gmail.com>
2025-07-21 20:12:13 -0500

    fix: put BIC 1st layer in separate envelope for Acts conversion

--- src/BarrelCalorimeterImaging_geo.cpp
+++ src/BarrelCalorimeterImaging_geo.cpp
@@ -62,43 +62,55 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector sens) {
   detector_physvol.addPhysVolID("system", detector_id);
   sdet.setPlacement(detector_physvol);
 
-  // build the envelope assembly
-  DetElement envelope_element("envelope", detector_id);
-  Assembly envelope_volume("envelope");
-  auto& envelope_params =
-      DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(envelope_element);
+  // Build the envelope assembly for layer 1
+  DetElement envelope1_element("envelope1", detector_id);
+  Assembly envelope1_volume("envelope1");
+  auto& envelope1_params =
+      DD4hepDetectorHelper::ensureExtension<dd4hep::rec::VariantParameters>(envelope1_element);
   if (x_detector.hasChild(_Unicode(envelope))) {
     xml_comp_t x_envelope = x_detector.child(_Unicode(envelope));
-    envelope_params.set<double>("envelope_r_min",
-                                getAttrOrDefault(x_envelope, _Unicode(envelope_r_min), 0.));
-    envelope_params.set<double>("envelope_r_max",
-                                getAttrOrDefault(x_envelope, _Unicode(envelope_r_max), 0.));
-    envelope_params.set<double>("envelope_z_min",
-                                getAttrOrDefault(x_envelope, _Unicode(envelope_z_min), 0.));
-    envelope_params.set<double>("envelope_z_max",
-                                getAttrOrDefault(x_envelope, _Unicode(envelope_z_max), 0.));
+    envelope1_params.set<double>("envelope_r_min",
+                                 getAttrOrDefault(x_envelope, _Unicode(envelope_r_min), 0.));
+    envelope1_params.set<double>("envelope_r_max",
+                                 getAttrOrDefault(x_envelope, _Unicode(envelope_r_max), 0.));
+    envelope1_params.set<double>("envelope_z_min",
+                                 getAttrOrDefault(x_envelope, _Unicode(envelope_z_min), 0.));
+    envelope1_params.set<double>("envelope_z_max",
+                                 getAttrOrDefault(x_envelope, _Unicode(envelope_z_max), 0.));
   }
   // Add the volume boundary material if configured
   for (xml_coll_t boundary_material(x_detector, _Unicode(boundary_material)); boundary_material;
        ++boundary_material) {
     xml_comp_t x_boundary_material = boundary_material;
-    DD4hepDetectorHelper::xmlToProtoSurfaceMaterial(x_boundary_material, envelope_params,
+    DD4hepDetectorHelper::xmlToProtoSurfaceMaterial(x_boundary_material, envelope1_params,
                                                     "boundary_material");
   }
   // Add the volume layer material if configured
   for (xml_coll_t layer_material(x_detector, _Unicode(layer_material)); layer_material;
        ++layer_material) {
     xml_comp_t x_layer_material = layer_material;
-    DD4hepDetectorHelper::xmlToProtoSurfaceMaterial(x_layer_material, envelope_params,
+    DD4hepDetectorHelper::xmlToProtoSurfaceMaterial(x_layer_material, envelope1_params,
                                                     "layer_material");
   }
 
-  // build a sector
-  DetElement sector_element("sector0", detector_id);
-  Assembly sector_volume("sector");
+  // Build the envelope assembly for layers 2 to 6 (no Acts support)
+  DetElement envelope26_element("envelope26", detector_id);
+  Assembly envelope26_volume("envelope26");
+
+  // Build a sector for layer 1
+  DetElement sector1_element("sector1", detector_id);
+  Assembly sector1_volume("sector1");
   if (x_detector.hasChild(_Unicode(sectors))) {
     xml_comp_t x_sectors = x_detector.child(_Unicode(sectors));
-    sector_volume.setVisAttributes(desc.visAttributes(x_sectors.visStr()));
+    sector1_volume.setVisAttributes(desc.visAttributes(x_sectors.visStr()));
+  }
+
+  // Build a sector for layers 2 to 6
+  DetElement sector26_element("sector26", detector_id);
+  Assembly sector26_volume("sector26");
+  if (x_detector.hasChild(_Unicode(sectors))) {
+    xml_comp_t x_sectors = x_detector.child(_Unicode(sectors));
+    sector26_volume.setVisAttributes(desc.visAttributes(x_sectors.visStr()));
   }
 
   // keep tracking of the total thickness
@@ -227,7 +239,7 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector sens) {
     }
   }
   if (x_detector.hasChild(_Unicode(support))) {
-    buildSupport(desc, sector_volume, x_detector.child(_Unicode(support)),
+    buildSupport(desc, sector1_volume, x_detector.child(_Unicode(support)),
                  {inner_r, layer_pos_z, x_dimensions.z(), half_dphi});
   }
   //Loop over the sets of layer elements in the detector.
@@ -257,7 +269,12 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector sens) {
       double layer_trd_z  = layer_thickness / 2; // account for frame
       Trapezoid layer_shape(layer_trd_x1, layer_trd_x2, layer_trd_y1, layer_trd_y2, layer_trd_z);
       Volume layer_volume(layer_name, layer_shape, layer_mat);
-      DetElement layer_element(sector_element, layer_name, detector_id);
+      DetElement layer_element(layer_name, detector_id);
+      if (layer_num == 1) {
+        sector1_element.add(layer_element);
+      } else {
+        sector26_element.add(layer_element);
+      }
 
       // Set region, limitset, and vis of layer.
       layer_volume.setAttributes(desc, x_layer.regionStr(), x_layer.limitsStr(), x_layer.visStr());
@@ -447,7 +464,9 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector sens) {
       }
 
       // Place layer into sector
-      PlacedVolume layer_physvol = sector_volume.placeVolume(layer_volume, layer_pos);
+      PlacedVolume layer_physvol = (layer_num == 1)
+                                       ? sector1_volume.placeVolume(layer_volume, layer_pos)
+                                       : sector26_volume.placeVolume(layer_volume, layer_pos);
       layer_physvol.addPhysVolID("layer", layer_num);
       layer_element.setPlacement(layer_physvol);
 
@@ -467,18 +486,32 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector sens) {
   for (int sector_num = 0; sector_num < nsides; sector_num++, phi -= dphi) {
     // Compute the sector position
     Transform3D tr(RotationZYX(0, phi, M_PI * 0.5), Translation3D(0, 0, 0));
-    PlacedVolume sector_physvol = envelope_volume.placeVolume(sector_volume, tr);
-    sector_physvol.addPhysVolID("sector", sector_num + 1);
-    DetElement sd =
-        (sector_num == 0) ? sector_element : sector_element.clone(Form("sector%d", sector_num));
-    sd.setPlacement(sector_physvol);
-    envelope_element.add(sd);
+
+    // Place 1st layer
+    PlacedVolume sector1_physvol = envelope1_volume.placeVolume(sector1_volume, tr);
+    sector1_physvol.addPhysVolID("sector", sector_num + 1);
+    DetElement sd1 =
+        (sector_num == 0) ? sector1_element : sector1_element.clone(Form("sector1_%d", sector_num));
+    sd1.setPlacement(sector1_physvol);
+    envelope1_element.add(sd1);
+
+    // Place layers 2 through 6
+    PlacedVolume sector26_physvol = envelope26_volume.placeVolume(sector26_volume, tr);
+    sector26_physvol.addPhysVolID("sector", sector_num + 1);
+    DetElement sd26 = (sector_num == 0) ? sector26_element
+                                        : sector26_element.clone(Form("sector26_%d", sector_num));
+    sd26.setPlacement(sector26_physvol);
+    envelope26_element.add(sd26);
   }
 
   // Place envelope
-  PlacedVolume envelope_physvol = detector_volume.placeVolume(envelope_volume);
-  envelope_element.setPlacement(envelope_physvol);
-  sdet.add(envelope_element);
+  PlacedVolume envelope1_physvol = detector_volume.placeVolume(envelope1_volume);
+  envelope1_element.setPlacement(envelope1_physvol);
+  sdet.add(envelope1_element);
+
+  PlacedVolume envelope26_physvol = detector_volume.placeVolume(envelope26_volume);
+  envelope26_element.setPlacement(envelope26_physvol);
+  sdet.add(envelope26_element);
 
   return sdet;
 }
