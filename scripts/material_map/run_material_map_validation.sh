46086a0d9b1b70d2d54942645d5224aa713cb809
Wouter Deconinck <wdconinc@gmail.com>
2025-07-28 13:54:06 -0500

    fix: disable material_mapping.py debugging

--- scripts/material_map/run_material_map_validation.sh
+++ scripts/material_map/run_material_map_validation.sh
@@ -144,9 +144,10 @@ echo "::group::----MAPPING------------"
 # input: geant4_material_tracks.root, geometry-map.json
 # output: material-maps.json or cbor. This is the material map that you want to provide to EICrecon, i.e.  -Pacts:MaterialMap=XXX  .Please --matFile to specify the name and type
 #         material-maps_tracks.root(recorded steps from geantino, for validation purpose)
-sed -i 's/acts\.logging\.INFO/acts.logging.VERBOSE/g' Examples/Scripts/Python/material_mapping.py
-sed -i 's/navigator = Navigator(/&level=acts.logging.VERBOSE,/' Examples/Scripts/Python/material_mapping.py
-python material_mapping_epic.py --xmlFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml --geoFile ${geoFile} --matFile ${matFile} | tail -n 500
+# The following two lines enable extensive debugging
+#sed -i 's/acts\.logging\.INFO/acts.logging.VERBOSE/g' Examples/Scripts/Python/material_mapping.py
+#sed -i 's/navigator = Navigator(/&level=acts.logging.VERBOSE,/' Examples/Scripts/Python/material_mapping.py
+python material_mapping_epic.py --xmlFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml --geoFile ${geoFile} --matFile ${matFile}
 echo "::endgroup::"
 
 echo "::group::----Prepare validation rootfile--------"
