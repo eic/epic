000a45ea2de572d1f3e030a1a3639c5e8964ffc7
Wouter Deconinck <wdconinc@gmail.com>
2025-07-30 22:48:35 -0500

    Revert "fix: disable material_mapping.py debugging"
    This reverts commit a04166fa2aa5cc292ec8411ebd2e96c521ac1a71.

--- scripts/material_map/run_material_map_validation.sh
+++ scripts/material_map/run_material_map_validation.sh
@@ -144,10 +144,9 @@ echo "::group::----MAPPING------------"
 # input: geant4_material_tracks.root, geometry-map.json
 # output: material-maps.json or cbor. This is the material map that you want to provide to EICrecon, i.e.  -Pacts:MaterialMap=XXX  .Please --matFile to specify the name and type
 #         material-maps_tracks.root(recorded steps from geantino, for validation purpose)
-# The following two lines enable extensive debugging
-#sed -i 's/acts\.logging\.INFO/acts.logging.VERBOSE/g' Examples/Scripts/Python/material_mapping.py
-#sed -i 's/navigator = Navigator(/&level=acts.logging.VERBOSE,/' Examples/Scripts/Python/material_mapping.py
-python material_mapping_epic.py --xmlFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml --geoFile ${geoFile} --matFile ${matFile}
+sed -i 's/acts\.logging\.INFO/acts.logging.VERBOSE/g' Examples/Scripts/Python/material_mapping.py
+sed -i 's/navigator = Navigator(/&level=acts.logging.VERBOSE,/' Examples/Scripts/Python/material_mapping.py
+python material_mapping_epic.py --xmlFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml --geoFile ${geoFile} --matFile ${matFile} | tail -n 500
 echo "::endgroup::"
 
 echo "::group::----Prepare validation rootfile--------"
