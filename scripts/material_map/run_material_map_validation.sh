f76bc99665b435c040ea3a3d9e0b5909c5f46809
Wouter Deconinck <wdconinc@gmail.com>
2025-08-01 09:21:45 -0500

    fix: propagator verbose too

--- scripts/material_map/run_material_map_validation.sh
+++ scripts/material_map/run_material_map_validation.sh
@@ -155,6 +155,7 @@ echo "::group::----MAPPING------------"
 #         material-maps_tracks.root(recorded steps from geantino, for validation purpose)
 sed -i 's/acts\.logging\.INFO/acts.logging.VERBOSE/g' Examples/Scripts/Python/material_mapping.py
 sed -i 's/navigator = Navigator($/&level=acts.logging.VERBOSE,/' Examples/Scripts/Python/material_mapping.py
+sed -i 's/propagator = Propagator(stepper, navigator/&, level=acts.logging.VERBOSE)/' Examples/Scripts/Python/material_mapping.py
 python material_mapping_epic.py --xmlFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml --geoFile ${geoFile} --matFile ${matFile} | tail -n 500
 echo "::endgroup::"
 
