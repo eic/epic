1abf7e4125b909a3a5b100b2de4d340a0e8eac03
Athavan Ramalingam <109119652+Gitavan@users.noreply.github.com>
2025-09-03 11:46:56 -0400

    Updated the PlotMaterialScan.C & PlotMaterialScan2D.C scripts

--- tools/PlotMaterialScan.C
+++ tools/PlotMaterialScan.C
@@ -1,8 +1,23 @@
 void PlotMaterialScan(string file="g4mat_scan.csv"){
 	TCanvas* c = new TCanvas("c", "Material Scan",0,0,700,600);
+	c->SetLeftMargin(0.15);  // increasing the canvas size in the given direction
+	c->SetBottomMargin(0.15); 
+	c->SetGrid();
+
+	gStyle->SetTitleFontSize(0.06);   // increase main title font size
+	gStyle->SetTitleFont(42, "");     // optional: change font
+
 	TGraph* g1d = new TGraph();
+	g1d->SetTitle("Material Scan; #eta; X/X_{0}"); // edit title name for L3 or L4
+	g1d->GetXaxis()->SetTitleSize(0.06);       // title and label sizes for the axis
+	g1d->GetYaxis()->SetTitleSize(0.06);
+	g1d->GetXaxis()->SetLabelSize(0.05);
+	g1d->GetYaxis()->SetLabelSize(0.05);
+	//g1d->GetXaxis()->SetLimits(-1,1);   // set eta range
+	g1d->GetXaxis()->SetTitleOffset(0.8); // Moves the axis title
+	g1d->GetYaxis()->SetTitleOffset(1.29);
+
 	TGraph* graphs_by_material[300];
-	g1d->SetTitle("Material Scan; eta; X/X0");
 	ifstream inputFile(file);
 	string MaterialNames[300];
 	int N_phi_points = 0;	
@@ -33,15 +48,17 @@ void PlotMaterialScan(string file="g4mat_scan.csv"){
 	i=0;
 	inputFile >> xxx >> eta >> phi >> air; 
 	do{
-		if(phi==0 && N_phi_points !=0){
-			for(int k=0; k<Nmat; ++k) {
+		if(phi==85 && N_phi_points !=0){   //change phi==0 for either L3 | 85 or L4 | 87 for stave material scan
+			for(int k=0; k<Nmat; ++k){
 				av_X0_by_material[k] /= (float)N_phi_points;
 				graphs_by_material[k]->SetPoint(i,prev_eta,av_X0_by_material[k]);
 			}	
 			avcX0 /= (float)N_phi_points;
-			g1d->SetPoint(i++,eta,avcX0);
+			g1d->SetPoint(i++,prev_eta,avcX0); 
+			g1d->SetLineWidth(2);
 			N_phi_points=0;		
 		}
+
 		float cumX0=0.0;
 		for (int k=0; k<Nmat; ++k){
 			inputFile >> X0_by_material[k];
@@ -49,20 +66,31 @@ void PlotMaterialScan(string file="g4mat_scan.csv"){
 		}
 		avcX0 += cumX0;
 		N_phi_points++;
-		for(int k=0; k<Nmat; ++k) 
+		for(int k=0; k<Nmat; ++k){ 
 			av_X0_by_material[k] +=  X0_by_material[k];
+		}
 		prev_eta=eta;
 		inputFile >> xxx >> eta >> phi >> air; 
-	} while (xxx != "lambda");
-	inputFile.close();
-	
+	} 	
+	while (xxx != "lambda");
+		inputFile.close();
         g1d->Draw();
-	TLegend* legend = new TLegend(0.1,0.8,0.4,0.95);
-        for(int k=0; k<Nmat; ++k){
-        	graphs_by_material[k]->SetLineColor(k+2);
-	        graphs_by_material[k]->Draw("L");
-	        string gr_name = graphs_by_material[k]->GetName();
-	        legend->AddEntry(gr_name.c_str(),MaterialNames[k].c_str(),"l");
+
+	// custom legend colors
+	int darkGreen = TColor::GetColor(0,200,0);   
+	int colors[] = {kBlue, kRed, darkGreen, kMagenta, kCyan, kOrange};
+	int Ncolors = sizeof(colors)/sizeof(int);
+
+	// change legend size and position for your liking
+	TLegend* legend = new TLegend(0.38,0.60,0.65,0.85);
+
+	legend->SetTextSize(0.035);
+	legend->SetTextFont(42); 
+		for(int k=0; k<Nmat; ++k){
+			graphs_by_material[k]->SetLineColor(colors[k % Ncolors]); //change to default colors with k+2 in colors[]
+			graphs_by_material[k]->SetLineWidth(2);
+			graphs_by_material[k]->Draw("L");
+			legend->AddEntry(graphs_by_material[k],MaterialNames[k].c_str(),"l");
 	}
 	legend->AddEntry("g1d","All","l");
 	legend->Draw();
--- tools/PlotMaterialScan2D.C
+++ tools/PlotMaterialScan2D.C
@@ -1,13 +1,21 @@
 void PlotMaterialScan2D(string file="g4mat_scan.csv"){
 	TCanvas* c = new TCanvas("c", "Material Scan",0,0,700,600);
+	c->SetRightMargin(0.21); // Z axis won't get cut off now
+	c->SetLeftMargin(0.11);
+	
 	TGraph2D* g = new TGraph2D();
-	g->SetTitle("Material Scan; eta; phi; X/X0");
+	g->SetTitle("Material Scan; #eta; #phi (#circ); X/X_{0}");
+	
+	gStyle->SetTitleFontSize(0.06);  //title size and font
+    gStyle->SetTitleFont(42, "");     
+	gStyle->SetPalette(kRainBow);     // slash out for default colours
+	
 	ifstream inputFile(file);
 	string MaterialNames[300];
 
 	int N_phi_points = 0;	
 	string xxx;
-	float eta,phi, air;
+	float eta, phi, air;
 	getline(inputFile,xxx);
 	stringstream ss(xxx);
 	string mat;
@@ -54,5 +62,35 @@ void PlotMaterialScan2D(string file="g4mat_scan.csv"){
 	g->SetNpy(360);
 	g->SetNpx(268);
 	g->Draw("COLZ");
+
+    gPad->Update();  
+
+    // Style axes 
+    TH1 *h2 = g->GetHistogram();
+    if (h2) {
+        h2->GetXaxis()->SetTitleSize(0.06);
+        h2->GetYaxis()->SetTitleSize(0.06);
+        h2->GetZaxis()->SetTitleSize(0.06);
+
+        h2->GetXaxis()->SetLabelSize(0.045);
+        h2->GetYaxis()->SetLabelSize(0.05);
+        h2->GetZaxis()->SetLabelSize(0.05);
+
+        h2->GetXaxis()->SetTitleOffset(0.8);
+        h2->GetYaxis()->SetTitleOffset(0.8);
+       // h2->GetZaxis()->SetTitleOffset(1);
+		h2->GetZaxis()->SetRangeUser(0.0, max*0.35); // Set you z-axis range
+    }
+
+    // Style the Z-axis color palette
+    TPaletteAxis *pal = (TPaletteAxis*)h2->GetListOfFunctions()->FindObject("palette");
+    if (pal) {
+        pal->SetLabelSize(0.05);
+        pal->SetTitleSize(0.06);
+        pal->SetTitleOffset(1.4);
+    }
+
+    c->Modified();
+    c->Update();
 	cout << "Maximum material thickness X/X0 = " << max << " at eta = " << eta_at_max << endl;
 }
