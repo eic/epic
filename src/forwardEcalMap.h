69e5da004f044af8e6cac62d1d8d2e5cbe2592c5
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-11-12 11:36:57 -0500

    src/forwardEcalMap.h: remove some unused constants

--- src/forwardEcalMap.h
+++ src/forwardEcalMap.h
@@ -25,16 +25,4 @@ static const int mNColBlock[mMaxNS][mMaxRowBlock] = {
     {3,  6,  9,  11, 12, 13, 14, 15, 16, 16, 17, 17, 18, 18, 18, 19, 19, 19, 17, 17,
      17, 19, 19, 19, 18, 18, 18, 17, 17, 16, 16, 15, 14, 13, 12, 11, 9,  6,  3}};
 
-static const int mNTowerInBlock        = 4;     //a block contains 4x4 towers
-static const double mBlockSize         = 10.0;  //size of block
-static const double mBlockLength       = 17.0;  //size of block
-static const double mSpaceBetweenBlock = 0.1;   //gap between blocks
 static const double mBackPlateZ        = 362.0; //Global Z of backplate = start of Hcal
-static const double mOffsetX[mMaxNS]   = {0.685 / 2.0,
-                                          0.685 / 2.0}; //gap between north and south halves
-static const double mOffsetY[mMaxNS]   = {
-    0.0, 0.0}; //height offset between beamline and middle of detector
-static const double mOffsetZ[mMaxNS] = {mBackPlateZ - mBlockLength, mBackPlateZ - mBlockLength};
-//z position of front face of detector
-static const double mOffsetXBeamPipe[mMaxNS] = {
-    7.5, 20.05}; //3 rows at beamline height is shifted, also = insert width
