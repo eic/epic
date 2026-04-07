087b693dca40262e89ee9a37026674251e7bdb27
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-04-07 12:58:31 -0400

    no if

--- cmake/setup.sh
+++ cmake/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-## Error if not the right name (this script is sourced, hence $1)
-if [[ "$(basename ${BASH_SOURCE[0]})" != "setup.sh" ]]; then
-        echo "Error: This script has ceased to exist at '$(realpath --no-symlinks ${BASH_SOURCE[0]})'."
-        echo "       Please use the version at '$(realpath --no-symlinks $(dirname ${BASH_SOURCE[0]})/bin/thisepic.sh)'."
-        return 1
-fi
+echo "Error: This script has ceased to exist at '$(realpath --no-symlinks ${BASH_SOURCE[0]})'."
+echo "       Please use the version at '$(realpath --no-symlinks $(dirname ${BASH_SOURCE[0]})/bin/thisepic.sh)'."
+return 1
